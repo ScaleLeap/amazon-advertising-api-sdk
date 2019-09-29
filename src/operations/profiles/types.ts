becdf2289be88d59cc220799b739b89d076c88ab
Roman Filippov <rf@romanfilippov.com>
2019-09-28 23:37:48 -0400

    style: run ESLint over the code

--- src/operations/profiles/types.ts
+++ src/operations/profiles/types.ts
@@ -6,17 +6,17 @@ export type ProfileId = t.TypeOf<typeof ProfileId>
 export const ProfileResponse = t.strict({
   /**
    * The ID of the profile that was updated, if successful.
-  */
+   */
   profileId: ProfileId,
 
   /**
    * An enumerated success or error code for machine use.
-  */
+   */
   code: t.string,
 
   /**
    * A human-readable description of the error, if unsuccessful.
-  */
+   */
   details: t.string,
 })
 export type ProfileResponse = t.TypeOf<typeof ProfileResponse>
@@ -53,10 +53,7 @@ export const TimeZone = t.union([
 ])
 export type TimeZone = t.TypeOf<typeof TimeZone>
 
-export const AccountInfoType = t.union([
-  t.literal('seller'),
-  t.literal('vendor'),
-])
+export const AccountInfoType = t.union([t.literal('seller'), t.literal('vendor')])
 export type AccountInfoType = t.TypeOf<typeof AccountInfoType>
 
 export const AccountInfo = t.strict({
@@ -64,22 +61,22 @@ export const AccountInfo = t.strict({
    * The string identifier for the marketplace associated with this profile.
    *
    * This is the same identifier used by MWS.
-  */
+   */
   description: t.string,
 
   /**
    * The string identifier for the ID associated with this account.
-  */
+   */
   id: t.string,
 
   /**
    * The string identifier for the account name.
-  */
+   */
   name: t.string,
 
   /**
    * The type of account being called.
-  */
+   */
   type: AccountInfoType,
 })
 export type AccountInfo = t.TypeOf<typeof AccountInfo>
@@ -87,33 +84,33 @@ export type AccountInfo = t.TypeOf<typeof AccountInfo>
 export const Profile = t.strict({
   /**
    * The ID of the profile.
-  */
+   */
   profileId: ProfileId,
 
   /**
    * The country code identifying the publisher(s) on which ads will run.
-  */
+   */
   countryCode: CountryCode,
 
   /**
    * The currency used for all monetary values for entities under this profile.
-  */
+   */
   currencyCode: CurrencyCode,
 
   /**
-  * The optional budget shared by all entities created under this profile.
-  * TODO: setup a check for minimums.
-  */
+   * The optional budget shared by all entities created under this profile.
+   * TODO: setup a check for minimums.
+   */
   dailyBudget: t.number,
 
   /**
    * The tz database time zone used for all date-based campaign management and reporting.
-  */
+   */
   timeZone: TimeZone,
 
   /**
    * Account info.
-  */
+   */
   accountInfo: AccountInfo,
 })
 export type Profile = t.TypeOf<typeof Profile>
