b100f39f7040b666d08898359d55dc87892c9bf5
Toan Nguyen <nguyentoandev@gmail.com>
2019-10-21 21:57:35 +0700

    test: refactor beforeEach

--- test/http-client.test.ts
+++ test/http-client.test.ts
@@ -15,17 +15,6 @@ describe('HttpClient', () => {
   })
 
   describe('get', () => {
-    beforeEach(() => {
-      const server = context.polly.server
-
-      server.host(SANDBOX_URI + '/', () => {
-        server.get('null-body').on('beforeResponse', (req, res) => {
-          res.status(HttpStatus.OK)
-          res.send('')
-        })
-      })
-    })
-
     it('should return a result', async () => {
       const res = await client.get('v2/profiles')
       expect(Array.isArray(res)).toBeTruthy()
@@ -36,33 +25,47 @@ describe('HttpClient', () => {
     })
 
     it('should throw NullError when response body is null', () => {
+      const server = context.polly.server
+
+      server.get(SANDBOX_URI + '/null-body').on('beforeResponse', (req, res) => {
+        res.status(HttpStatus.OK)
+        res.send('')
+      })
+
       return expect(client.get('null-body')).rejects.toThrow(NullError)
     })
 
     it('should throw a ResourceNotFoundError when resource is not found', () => {
+      const server = context.polly.server
+
+      server.get(SANDBOX_URI + '/foobar').on('beforeResponse', (req, res) => {
+        res.status(HttpStatus.NOT_FOUND)
+        res.send({
+          code: 'NOT_FOUND',
+          details: 'Could not find resource for full path: https://advertising-api-test.amazon.com/foobar'
+        })
+      })
+
       return expect(client.get('foobar')).rejects.toThrow(ResourceNotFoundError)
     })
   })
 
   describe('download', () => {
-    beforeEach(() => {
+    it('should throw if location header not set', async () => {
       const server = context.polly.server
-      server.host(SANDBOX_URI + '/', () => {
-        server.get('profiles').on('beforeResponse', (req, res) => {
-          res.setHeader('Location', '')
-          res.status(HttpStatus.OK)
-          res.send({
-            snapshotId: 'amzn1.clicksAPI.v1.p1.5C8B19EB.7298de0e-17cd-441f-bf5c-17a27406b0d6',
-            status: 'SUCCESS',
-            statusDetails: 'Snapshot has been successfully generated.',
-            location: '',
-            fileSize: 518,
-          })
+
+      server.get(SANDBOX_URI + '/profiles').on('beforeResponse', (req, res) => {
+        res.setHeader('Location', '')
+        res.status(HttpStatus.OK)
+        res.send({
+          snapshotId: 'amzn1.clicksAPI.v1.p1.5C8B19EB.7298de0e-17cd-441f-bf5c-17a27406b0d6',
+          status: 'SUCCESS',
+          statusDetails: 'Snapshot has been successfully generated.',
+          location: '',
+          fileSize: 518,
         })
       })
-    })
 
-    it('should throw if location header not set', async () => {
       const promise = client.download('profiles')
       return expect(promise).rejects.toThrowError(InvalidProgramStateError)
     })
