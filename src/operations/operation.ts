26b464173f0b1c0f05eb22c1d63d7dd64033776c
Toan Nguyen <nguyentoandev@gmail.com>
2019-10-26 16:36:46 +0700

    refactor: remove contructors of implement operator

--- src/operations/operation.ts
+++ src/operations/operation.ts
@@ -1,3 +1,6 @@
+import { HttpClient } from '../http-client'
+
 export class Operation {
   protected version = 'v2'
+  constructor(protected client: HttpClient) {}
 }
--- src/operations/profiles/ProfileOperation.ts
+++ src/operations/profiles/ProfileOperation.ts
@@ -1,12 +1,8 @@
 import { Operation } from '../operation'
-import { HttpClient } from '../../http-client'
 import { Profile } from './types'
 
 export class ProfileOperation extends Operation {
   protected resource = 'profiles'
-  constructor(protected client: HttpClient) {
-    super()
-  }
 
   public listProfiles(): Promise<Profile[]> {
     return this.client.get<Profile[]>(`${this.version}/${this.resource}`)
