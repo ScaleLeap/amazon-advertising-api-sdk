664e45b1c588ca39f7ae530879ecfa98b00408ac
Toan Nguyen <nguyentoandev@gmail.com>
2019-10-28 23:06:32 +0700

    feature: validate the output, before returning

---
+++ src/decorators.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2019, Scale Leap
+ */
+
+import * as t from 'io-ts'
+import * as tPromise from 'io-ts-promise'
+
+import { Operation } from './operations/operation'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Descriptor = TypedPropertyDescriptor<any>
+type Decoder = t.Mixed
+
+export function Decode(decoder: Decoder): Function {
+  return (target: Operation, propertyKey: string, descriptor: Descriptor) => {
+    const originalMethod = descriptor.value
+
+    return {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      value: function value(...args: any[]) {
+        return Promise.resolve(originalMethod.apply(this, args)).then(res =>
+          tPromise.decode(decoder, res),
+        )
+      },
+    }
+  }
+}
+
+export function DecodeArray(decoder: Decoder): Function {
+  return Decode(t.array(decoder))
+}
--- src/operations/profiles/ProfileOperation.ts
+++ src/operations/profiles/ProfileOperation.ts
@@ -1,9 +1,11 @@
 import { Operation } from '../operation'
 import { Profile } from './types'
+import { DecodeArray } from '../../decorators'
 
 export class ProfileOperation extends Operation {
   protected resource = 'profiles'
 
+  @DecodeArray(Profile)
   public listProfiles(): Promise<Profile[]> {
     return this.client.get<Profile[]>(`${this.version}/${this.resource}`)
   }
