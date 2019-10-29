c63a6f9d0975adacd0d60d8234ba9899e3eb3f8c
Toan Nguyen <nguyentoandev@gmail.com>
2019-10-29 22:42:31 +0700

    feature: implement getProfile and updateProfiles operator

--- test/ops-profiles.test.ts
+++ test/ops-profiles.test.ts
@@ -7,14 +7,56 @@ import setupPolly from './polly'
 setupPolly()
 
 describe('ProfileOperation', () => {
+  const client = httpClientFactory()
+  const operationProvider = new OperationProvider(client)
+  const profileOperation = operationProvider.create(ProfileOperation)
+  const TEST_PROFILE_ID = 2984328618318898
+
   describe('listProfiles', () => {
     it('should return an array or profiles', async () => {
-      const client = httpClientFactory()
-      const operationProvider = new OperationProvider(client)
-      const profileOperation = operationProvider.create(ProfileOperation)
       const res: Profile[] = await profileOperation.listProfiles()
 
       expect(Array.isArray(res)).toBeTruthy()
+    })
+  })
+
+  describe('getProfile', () => {
+    it('should return a profile object', async () => {
+      const profile = await profileOperation.getProfile(TEST_PROFILE_ID)
+
+      expect(profile).toBeTruthy()
+
+      if (profile) {
+        expect(profile.profileId).toBe(TEST_PROFILE_ID)
+      }
+    })
+  })
+
+  describe('updateProfiles', () => {
+    it('should update the profile', async () => {
+      const dailyBudget = 340
+
+      const res = await profileOperation.updateProfiles([
+        {
+          profileId: TEST_PROFILE_ID,
+          dailyBudget,
+        },
+      ])
+
+      expect(res).toBeTruthy()
+      expect(res).toHaveLength(1)
+
+      if (res && res[0]) {
+        expect(res[0].code).toBe('SUCCESS')
+        expect(res[0].profileId).toBe(TEST_PROFILE_ID)
+      }
+
+      const profile = await profileOperation.getProfile(TEST_PROFILE_ID)
+      expect(profile).toBeTruthy()
+
+      if (profile) {
+        expect(profile.dailyBudget).toBe(dailyBudget)
+      }
     })
   })
 })
