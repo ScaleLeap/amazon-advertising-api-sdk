ef08e55b83de66ae19ec2c3f821f989ebcad0a85
Toan Nguyen <nguyentoandev@gmail.com>
2019-10-29 22:42:31 +0700

    feature: implement getProfile and updateProfiles operator

--- src/operations/profiles/ops-profiles.ts
+++ src/operations/profiles/ops-profiles.ts
@@ -1,12 +1,21 @@
 import { Operation } from '../operation'
-import { Profile } from './types'
-import { DecodeArray } from '../../decorators'
-
+import { Profile, ProfileId, ProfileResponse } from './types'
+import { Decode, DecodeArray } from '../../decorators'
 export class ProfileOperation extends Operation {
   protected resource = 'profiles'
 
   @DecodeArray(Profile)
   public listProfiles(): Promise<Profile[]> {
     return this.client.get<Profile[]>(`${this.version}/${this.resource}`)
+  }
+
+  @Decode(ProfileId)
+  public getProfile(profileId: ProfileId): Promise<Profile> {
+    return this.client.get<Profile>(`${this.version}/${this.resource}/${profileId}`)
+  }
+
+  @Decode(Profile)
+  public updateProfiles(profiles: Partial<Profile>[]): Promise<ProfileResponse[]> {
+    return this.client.put<ProfileResponse[]>(`${this.version}/${this.resource}`, profiles)
   }
 }
