874c4bac404daef39baf584af5cfddc9e88c8edb
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-04 15:01:48 +0700

    feature: apply implicit return for operators

--- src/operations/profiles/ops-profiles.ts
+++ src/operations/profiles/ops-profiles.ts
@@ -5,17 +5,14 @@ export class ProfileOperation extends Operation {
   protected resource = 'profiles'
 
   @DecodeArray(Profile)
-  public listProfiles(): Promise<Profile[]> {
-    return this.client.get<Profile[]>(`${this.version}/${this.resource}`)
-  }
+  public listProfiles = (): Promise<Profile[]> =>
+    this.client.get<Profile[]>(`${this.version}/${this.resource}`)
 
   @Decode(Profile)
-  public getProfile(profileId: ProfileId): Promise<Profile> {
-    return this.client.get<Profile>(`${this.version}/${this.resource}/${profileId}`)
-  }
+  public getProfile = (profileId: ProfileId): Promise<Profile> =>
+    this.client.get<Profile>(`${this.version}/${this.resource}/${profileId}`)
 
   @DecodeArray(ProfileResponse)
-  public updateProfiles(profiles: Partial<Profile>[]): Promise<ProfileResponse[]> {
-    return this.client.put<ProfileResponse[]>(`${this.version}/${this.resource}`, profiles)
-  }
+  public updateProfiles = (profiles: Partial<Profile>[]): Promise<ProfileResponse[]> =>
+    this.client.put<ProfileResponse[]>(`${this.version}/${this.resource}`, profiles)
 }
