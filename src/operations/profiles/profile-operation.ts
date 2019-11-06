8f12646dbe4c6e55e181282292863faaa16ffd51
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-06 23:27:09 +0700

    chore: rename profile-operation file

--- src/operations/profiles/profile-operation.ts
+++ src/operations/profiles/profile-operation.ts
@@ -1,12 +1,45 @@
 import { Operation } from '../operation'
-import { Profile } from './types'
-import { DecodeArray } from '../../decorators'
+import {
+  Profile,
+  ProfileId,
+  RegisterBrand,
+  ProfileResponse,
+  RegisterProfileResponse,
+  ProfileRegistrationResponse,
+} from './types'
+import { Decode, DecodeArray } from '../../decorators'
+import { CountryCode } from '../commons/types'
 
 export class ProfileOperation extends Operation {
   protected resource = 'profiles'
 
   @DecodeArray(Profile)
-  public listProfiles(): Promise<Profile[]> {
+  public listProfiles() {
     return this.client.get<Profile[]>(`${this.version}/${this.resource}`)
+  }
+
+  @Decode(Profile)
+  public getProfile(profileId: ProfileId) {
+    return this.client.get<Profile>(`${this.version}/${this.resource}/${profileId}`)
+  }
+
+  @DecodeArray(ProfileResponse)
+  public updateProfiles(profiles: Partial<Profile>[]) {
+    return this.client.put<ProfileResponse[]>(`${this.version}/${this.resource}`, profiles)
+  }
+
+  @Decode(RegisterProfileResponse)
+  public registerProfile(countryCode: CountryCode = 'US') {
+    return this.client.put<RegisterProfileResponse>(`${this.version}/${this.resource}/register`, {
+      countryCode,
+    })
+  }
+
+  @Decode(ProfileRegistrationResponse)
+  public registerBrand(registerBrand: RegisterBrand) {
+    return this.client.put<ProfileRegistrationResponse>(
+      `${this.version}/${this.resource}/registerBrand`,
+      registerBrand,
+    )
   }
 }
