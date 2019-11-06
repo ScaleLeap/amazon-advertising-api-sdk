4a60d7602033422a4038e931c35a663324039dfe
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-06 23:37:44 +0700

    chore: rename portfolio operation

--- test/portfolio-operation.test.ts
+++ test/portfolio-operation.test.ts
@@ -1,7 +1,5 @@
 import { OperationProvider } from '../src/operations/operation-provider'
-import { CurrencyCode } from '../src/operations/commons/types'
-import { PortfolioOperation } from '../src/operations/portfolios/ops-portfolios'
-import { PortfolioState } from '../src/operations/portfolios/types'
+import { PortfolioOperation } from '../src/operations/portfolios/portfolio-operation'
 import { httpClientFactory } from './http-client-factory'
 import setupPolly from './polly'
 
