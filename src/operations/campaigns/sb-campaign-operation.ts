1d40627a15f7d8645434b488b602ee7c21452fa9
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-10 14:26:08 +0700

    feature: remove PortfolioID params of create Campaign operation

--- src/operations/campaigns/sb-campaign-operation.ts
+++ src/operations/campaigns/sb-campaign-operation.ts
@@ -3,7 +3,6 @@ import { Decode, DecodeArray } from '../../decorators'
 import { AmazonAdTypeURIPrefix } from '../amazon-ad-type-uri-prefix'
 
 import { Campaign, ListCampaignsParams, CampaignId, CampaignResponse } from './types'
-import { PortfolioId } from '../portfolios/types'
 
 export class SponsoredBrandsCampaignOperation extends Operation {
   protected resource = `${this.version}/${AmazonAdTypeURIPrefix.SponsoredBrands}/campaigns`
@@ -19,7 +18,7 @@ export class SponsoredBrandsCampaignOperation extends Operation {
   }
 
   @DecodeArray(CampaignResponse)
-  public createCampaigns(porfolioId: PortfolioId, campaigns: Campaign[]) {
-    return this.client.post<CampaignResponse[]>(this.resource, { porfolioId, campaigns })
+  public createCampaigns(campaigns: Campaign[]) {
+    return this.client.post<CampaignResponse[]>(this.resource, campaigns)
   }
 }
