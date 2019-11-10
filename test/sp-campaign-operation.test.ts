8e1e4a2a47830fd0a160769084ec8894ce1600d3
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-10 21:44:50 +0700

    test: Update recordings structure and skip decode response to get listCampaigns extended recordings

--- test/sp-campaign-operation.test.ts
+++ test/sp-campaign-operation.test.ts
@@ -16,60 +16,58 @@ describe('SponsoredProductsCampaignOperation', () => {
   const campaignOperation = operationProvider.create(SponsoredProductsCampaignOperation)
   const CAMPAIGN_ID = 31299234922913
 
-  describe('Sponsored Brands', () => {
-    describe('listCampaigns', () => {
-      it('should return an array of campaigns', async () => {
-        const res = await campaignOperation.listCampaigns()
-        expect(Array.isArray(res)).toBeTruthy()
-        expect(typeof res[0].name).toBe('string')
-        expect(res[0]).toHaveProperty('bidding')
-      })
+  describe('listCampaigns', () => {
+    it('should return an array of campaigns', async () => {
+      const res = await campaignOperation.listCampaigns()
+      expect(Array.isArray(res)).toBeTruthy()
+      expect(typeof res[0].name).toBe('string')
+      expect(res[0]).toHaveProperty('bidding')
+    })
+  })
+
+  describe('listCampaignsEx', () => {
+    it('should return an array of expanded campaigns', async () => {
+      const res = await campaignOperation.listCampaignsEx()
+      expect(Array.isArray(res)).toBeTruthy()
+      expect(typeof res[0].name).toBe('string')
+      expect(res[0]).toHaveProperty('bidding')
     })
 
-    describe('listCampaignsEx', () => {
-      it('should return an array of expanded campaigns', async () => {
-        const res = await campaignOperation.listCampaignsEx()
-        expect(Array.isArray(res)).toBeTruthy()
-        expect(typeof res[0].name).toBe('string')
-        expect(res[0]).toHaveProperty('bidding')
+    it('should return a filtered list of results', async () => {
+      const res = await campaignOperation.listCampaignsEx({
+        campaignIdFilter: [CAMPAIGN_ID],
       })
-
-      it('should return a filtered list of results', async () => {
-        const res = await campaignOperation.listCampaignsEx({
-          campaignIdFilter: [CAMPAIGN_ID],
-        })
-        expect(Array.isArray(res)).toBeTruthy()
-        expect(res[0].campaignId).toBe(CAMPAIGN_ID)
-      })
+      expect(Array.isArray(res)).toBeTruthy()
+      expect(res[0].campaignId).toBe(CAMPAIGN_ID)
     })
+  })
 
-    describe('getCampaign', () => {
-      it('should return a single campaign', async () => {
-        const res = await campaignOperation.getCampaign(CAMPAIGN_ID)
-        expect(res.campaignId).toBe(CAMPAIGN_ID)
-        expect(res).toHaveProperty('bidding')
-      })
+  describe('getCampaign', () => {
+    it('should return a single campaign', async () => {
+      const res = await campaignOperation.getCampaign(CAMPAIGN_ID)
+      expect(res.campaignId).toBe(CAMPAIGN_ID)
+      expect(res).toHaveProperty('bidding')
     })
+  })
 
-    describe('createCampaigns', () => {
-      it('should create a campaign', async () => {
-        const res = await campaignOperation.createCampaigns([
-          {
-            name: 'test campaign 4',
-            campaignType: CampaignType.value,
-            dailyBudget: 1,
-            state: CampaignState.types[0].value,
-            targetingType: CampaignTargetingType.types[0].value,
-            startDate: new Date()
-              .toISOString()
-              .slice(0, 10)
-              .replace(/-/g, ''),
-            premiumBidAdjustment: true,
-          },
-        ])
+  describe('createCampaigns', () => {
+    it('should create a campaign', async () => {
+      const res = await campaignOperation.createCampaigns([
+        {
+          name: 'test campaign 4',
+          campaignType: CampaignType.value,
+          dailyBudget: 1,
+          state: CampaignState.types[0].value,
+          targetingType: CampaignTargetingType.types[0].value,
+          startDate: new Date()
+            .toISOString()
+            .slice(0, 10)
+            .replace(/-/g, ''),
+          premiumBidAdjustment: true,
+        },
+      ])
 
-        expect(Array.isArray(res)).toBeTruthy()
-      })
+      expect(Array.isArray(res)).toBeTruthy()
     })
   })
 
