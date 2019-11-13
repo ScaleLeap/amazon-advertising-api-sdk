4644cdb3cafa38b721ac34aa03a5029548b83285
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-13 21:54:06 +0700

    feature: implement getPortfolio and getPortfolioEx operations

--- src/operations/portfolios/portfolio-operation.ts
+++ src/operations/portfolios/portfolio-operation.ts
@@ -1,6 +1,6 @@
 import { Operation } from '../operation'
-import { ListPortfoliosParams, Portfolio, PortfolioExtended } from './types'
-import { DecodeArray } from '../../decorators'
+import { ListPortfoliosParams, Portfolio, PortfolioExtended, PortfolioId } from './types'
+import { Decode, DecodeArray } from '../../decorators'
 
 export class PortfolioOperation extends Operation {
   protected resource = `${this.version}/portfolios`
@@ -13,5 +13,15 @@ export class PortfolioOperation extends Operation {
   @DecodeArray(PortfolioExtended)
   public listPortfoliosEx(params?: ListPortfoliosParams) {
     return this.client.get<PortfolioExtended[]>(this.query(`${this.resource}/extended`, params))
+  }
+
+  @Decode(Portfolio)
+  public getPortfolio(portfolioId: PortfolioId) {
+    return this.client.get<Portfolio>(`${this.resource}/${portfolioId}`)
+  }
+
+  @Decode(PortfolioExtended)
+  public getPortfolioEx(portfolioId: PortfolioId) {
+    return this.client.get<PortfolioExtended>(`${this.resource}/extended/${portfolioId}`)
   }
 }
