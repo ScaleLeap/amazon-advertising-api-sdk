2bf9a5155cf2f5d16c4c32fba5d58a69db8e2b50
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-21 22:03:42 +0700

    feature: implement SP report operation requestReport method

--- src/operations/reports/sp/sp-campaign-report-request.ts
+++
@@ -1,8 +0,0 @@
-import { BaseReportRequest } from '../base-report-request'
-import { SponsoredProductsReportTypeEnum } from '../report-types-enum'
-import { CampaignReportMetricsEnum } from '../metrics/campaign-report-metrics-enum'
-
-export interface SponsoredProductsCampaignReportRequest
-  extends BaseReportRequest<SponsoredProductsReportTypeEnum, CampaignReportMetricsEnum> {
-  recordType: SponsoredProductsReportTypeEnum.CAMPAIGNS
-}
