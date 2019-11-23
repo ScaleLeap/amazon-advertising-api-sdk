625f5fbbcc85492d1524898c4ceacc4e029a6d68
Toan Nguyen <nguyentoandev@gmail.com>
2019-11-23 15:49:16 +0700

    feature: implement getReport operation and refactor sp report Operation

--- src/operations/reports/sp/sp-report-operation.ts
+++ src/operations/reports/sp/sp-report-operation.ts
@@ -1,14 +1,14 @@
 import { omit } from 'lodash'
 import { Decode } from '../../../decorators'
-import { BaseReportOperation } from '../base-report-operation'
-import { ReportResponse } from '../report-response'
+import { Operation } from '../../operation'
+import { ReportResponse, ReportId } from '../report-response'
 import { SponsoredProductsCampaignReportParams } from './sp-campaign-report-params'
 
 type SponsoredProductsReportParams = SponsoredProductsCampaignReportParams
 
 export class SponsoredProductsReportOperation<
   ReportParams extends SponsoredProductsReportParams
-> extends BaseReportOperation {
+> extends Operation {
   private type = 'sp'
 
   @Decode(ReportResponse)
@@ -17,5 +17,10 @@ export class SponsoredProductsReportOperation<
       `${this.version}/${this.type}/${params.recordType}/report`,
       this.paramsFilterTransformerReal(omit(params, ['recordType']), ['metrics']),
     )
+  }
+
+  @Decode(ReportResponse)
+  public getReport(reportId: ReportId) {
+    return this.client.get<ReportResponse>(`${this.version}/reports/${reportId}`)
   }
 }
