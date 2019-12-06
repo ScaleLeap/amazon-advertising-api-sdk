52181269735e40e34e9ae52e05a3f63a085dc911
Toan Nguyen <nguyentoandev@gmail.com>
2019-12-06 23:10:52 +0700

    test: write test code for adgroup, keyword, product ads, target report

--- test/reports/sp/sp-report-operation.test.ts
+++ test/reports/sp/sp-report-operation.test.ts
@@ -7,6 +7,10 @@ import { SponsoredProductsReportTypeEnum } from '../../../src/operations/reports
 import { CampaignReportMetricsEnum } from '../../../src/operations/reports/metrics/campaign-report-metrics-enum'
 import { ReportResponseStatusEnum } from '../../../src/operations/reports/report-response'
 import { DateTimeUtils } from '../../datetime-utils'
+import { AdGroupReportMetricsEnum } from '../../../src/operations/reports/metrics/adgroup-report-metrics-enum'
+import { KeywordReportMetricsEnum } from '../../../src/operations/reports/metrics/keyword-report-metrics-enum'
+import { ProductAdsReportMetricsEnum } from '../../../src/operations/reports/metrics/product-ads-report-metrics-enum'
+import { ProductTargetingReportMetricsEnum } from '../../../src/operations/reports/metrics/product-targeting-report-metrics-enum'
 
 setupPolly()
 
@@ -20,6 +24,81 @@ describe('SponsoredProductsReportOperation', () => {
       const res = await reportOperation.requestReport({
         recordType: SponsoredProductsReportTypeEnum.CAMPAIGNS,
         metrics: [CampaignReportMetricsEnum.ATTRIBUTED_SALES14D],
+        reportDate: DateTimeUtils.getCurrentISODate(),
+      })
+
+      expect(res.reportId).toBeDefined()
+      expect(res.recordType).toBeDefined()
+      expect(res.status).toBe(ReportResponseStatusEnum.IN_PROGRESS)
+      expect(res.statusDetails).toBeDefined()
+    })
+
+    it(`should return a in progress status with adgroups report ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await reportOperation.requestReport({
+        recordType: SponsoredProductsReportTypeEnum.AD_GROUPS,
+        metrics: [
+          AdGroupReportMetricsEnum.CAMPAIGN_ID,
+          AdGroupReportMetricsEnum.CAMPAIGN_NAME,
+          AdGroupReportMetricsEnum.ADGROUP_ID,
+          AdGroupReportMetricsEnum.ADGROUP_NAME,
+          AdGroupReportMetricsEnum.COST,
+          AdGroupReportMetricsEnum.IMPRESSIONS,
+        ],
+        reportDate: DateTimeUtils.getCurrentISODate(),
+      })
+
+      expect(res.reportId).toBeDefined()
+      expect(res.recordType).toBeDefined()
+      expect(res.status).toBe(ReportResponseStatusEnum.IN_PROGRESS)
+      expect(res.statusDetails).toBeDefined()
+    })
+
+    it(`should return a in progress status with keywords report ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await reportOperation.requestReport({
+        recordType: SponsoredProductsReportTypeEnum.KEYWORDS,
+        metrics: [
+          KeywordReportMetricsEnum.CAMPAIGN_ID,
+          KeywordReportMetricsEnum.KEYWORD_ID,
+          KeywordReportMetricsEnum.COST,
+          KeywordReportMetricsEnum.IMPRESSIONS,
+        ],
+        reportDate: DateTimeUtils.getCurrentISODate(),
+      })
+
+      expect(res.reportId).toBeDefined()
+      expect(res.recordType).toBeDefined()
+      expect(res.status).toBe(ReportResponseStatusEnum.IN_PROGRESS)
+      expect(res.statusDetails).toBeDefined()
+    })
+
+    it(`should return a in progress status with product ads report ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await reportOperation.requestReport({
+        recordType: SponsoredProductsReportTypeEnum.PRODUCT_ADS,
+        metrics: [
+          ProductAdsReportMetricsEnum.CAMPAIGN_ID,
+          ProductAdsReportMetricsEnum.COST,
+          ProductAdsReportMetricsEnum.IMPRESSIONS,
+          ProductAdsReportMetricsEnum.SKU,
+          ProductAdsReportMetricsEnum.ASIN,
+        ],
+        reportDate: DateTimeUtils.getCurrentISODate(),
+      })
+
+      expect(res.reportId).toBeDefined()
+      expect(res.recordType).toBeDefined()
+      expect(res.status).toBe(ReportResponseStatusEnum.IN_PROGRESS)
+      expect(res.statusDetails).toBeDefined()
+    })
+
+    it(`should return a in progress status with targets report ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await reportOperation.requestReport({
+        recordType: SponsoredProductsReportTypeEnum.PRODUCT_ATTRIBUTE_TARGETING,
+        metrics: [
+          ProductTargetingReportMetricsEnum.CAMPAIGN_ID,
+          ProductTargetingReportMetricsEnum.TARGET_ID,
+          ProductTargetingReportMetricsEnum.COST,
+          ProductTargetingReportMetricsEnum.IMPRESSIONS,
+        ],
         reportDate: DateTimeUtils.getCurrentISODate(),
       })
 
