36a328bba2d4950e4d3dff1df963793c4519ebd8
Toan Nguyen <nguyentoandev@gmail.com>
2019-12-28 17:18:46 +0700

    test: add createKeywords method test

--- test/operations/keywords/sp-ad-group-keywords-operation.test.ts
+++ test/operations/keywords/sp-ad-group-keywords-operation.test.ts
@@ -3,7 +3,13 @@ import { SponsoredProductsAdGroupKeywordsOperation } from '../../../src/operatio
 import { POLLY_PASSTHROUGH_TAG } from '../../constants'
 import { httpClientFactory } from '../../http-client-factory'
 import { OperationProvider } from '../../../src'
-import { Keyword } from '../../../src/operations/keywords/types'
+import {
+  Keyword,
+  CreateKeywordsParam,
+  KeywordMatchTypeEnum,
+  KeywordStateEnum,
+  KeywordResponseStatusEnum,
+} from '../../../src/operations/keywords/types'
 
 setupPolly()
 
@@ -11,9 +17,32 @@ describe('SponsoredProductsAdGroupKeywordsOperation', () => {
   const client = httpClientFactory()
   const operationProvider = new OperationProvider(client)
   const operation = operationProvider.create(SponsoredProductsAdGroupKeywordsOperation)
+  const PAUSED_CAMPAIGN_ID = 31299234922913
+  const PAUSED_AD_GROUP_ID = 109226894492069
+
+  describe('createKeywords', () => {
+    it(`should create a keyword ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: CreateKeywordsParam[] = [
+        {
+          campaignId: PAUSED_CAMPAIGN_ID,
+
+          adGroupId: PAUSED_AD_GROUP_ID,
+
+          keywordText: 'Apple',
+
+          matchType: KeywordMatchTypeEnum.BROAD,
+
+          state: KeywordStateEnum.PAUSED,
+        },
+      ]
+      const [res] = await operation.createKeywords(params)
+
+      expect(res.code).toEqual(KeywordResponseStatusEnum.SUCCESS)
+    })
+  })
 
   describe('listBiddableKeywords', () => {
-    it(`should return an array of Keywords  ${POLLY_PASSTHROUGH_TAG}`, async () => {
+    it(`should return an array of Keywords ${POLLY_PASSTHROUGH_TAG}`, async () => {
       const res: Keyword[] = await operation.listBiddableKeywords()
 
       expect(Array.isArray(res)).toBeTruthy()
