cac9a20ba0b52cbf30623d4a98f2376155c15f06
Toan Nguyen <nguyentoandev@gmail.com>
2020-01-03 21:06:01 +0700

    test: add getNegativeKeyword, getNegativeKeywordExtended, updateKeywords test code

--- test/operations/keywords/sp-ad-group-negative-keywords-operation.test.ts
+++ test/operations/keywords/sp-ad-group-negative-keywords-operation.test.ts
@@ -8,6 +8,7 @@ import {
   KeywordStateEnum,
   NegativeKeywordResponseStatusEnum,
   NegativeKeywordMatchTypeEnum,
+  UpdateNegativeKeywordsParam,
 } from '../../../src/operations/keywords/types'
 
 setupPolly()
@@ -18,6 +19,27 @@ describe('SponsoredProductsAdGroupNegativeKeywordsOperation', () => {
   const operation = operationProvider.create(SponsoredProductsAdGroupNegativeKeywordsOperation)
   const MANUAL_CAMPAIGN_ID = 164069484151709
   const MANUAL_AD_GROUP_ID = 149522344269714
+  const KEYWORD_ID = 262433850080632
+
+  describe('getNegativeKeyword', () => {
+    it(`should return a Negative Keyword ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getNegativeKeyword(KEYWORD_ID)
+
+      expect(res.campaignId).toBe(MANUAL_CAMPAIGN_ID)
+      expect(res.adGroupId).toBe(MANUAL_AD_GROUP_ID)
+      expect(res.keywordId).toBe(KEYWORD_ID)
+    })
+  })
+
+  describe('getNegativeKeywordExtended', () => {
+    it(`should return a Negative Keyword Extended ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getNegativeKeywordExtended(KEYWORD_ID)
+
+      expect(res.campaignId).toBe(MANUAL_CAMPAIGN_ID)
+      expect(res.adGroupId).toBe(MANUAL_AD_GROUP_ID)
+      expect(res.keywordId).toBe(KEYWORD_ID)
+    })
+  })
 
   describe.skip('createNegativeKeywords', () => {
     it(`should create a negative keyword ${POLLY_PASSTHROUGH_TAG}`, async () => {
@@ -35,6 +57,20 @@ describe('SponsoredProductsAdGroupNegativeKeywordsOperation', () => {
         },
       ]
       const [res] = await operation.createNegativeKeywords(params)
+
+      expect(res.code).toEqual(NegativeKeywordResponseStatusEnum.SUCCESS)
+    })
+  })
+
+  describe('updateKeywords', () => {
+    it(`should update a negative keyword ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: UpdateNegativeKeywordsParam[] = [
+        {
+          keywordId: KEYWORD_ID,
+          state: KeywordStateEnum.PAUSED,
+        },
+      ]
+      const [res] = await operation.updateNegativeKeywords(params)
 
       expect(res.code).toEqual(NegativeKeywordResponseStatusEnum.SUCCESS)
     })
