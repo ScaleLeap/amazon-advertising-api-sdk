8dff7d5dcc26509d46db44a239a217ba69bf557c
Toan Nguyen <nguyentoandev@gmail.com>
2020-01-04 20:41:13 +0700

    test: get and list campaign negative keyword

--- test/operations/keywords/sp-campaign-negative-keywords-operation.test.ts
+++ test/operations/keywords/sp-campaign-negative-keywords-operation.test.ts
@@ -8,6 +8,9 @@ import {
   CampaignNegativeKeywordMatchTypeEnum,
   CampaignNegativeKeywordStateEnum,
   CampaignNegativeKeywordResponseStatusEnum,
+  CampaignNegativeKeyword,
+  CampaignNegativeKeywordExtended,
+  ListCampaignNegativeKeywordsParam,
 } from '../../../src/operations/keywords/types'
 
 setupPolly()
@@ -17,15 +20,38 @@ describe('SponsoredProductsCampaignNegativeKeywordsOperation', () => {
   const operationProvider = new OperationProvider(client)
   const operation = operationProvider.create(SponsoredProductsCampaignNegativeKeywordsOperation)
   const MANUAL_CAMPAIGN_ID = 164069484151709
-  const NEGATIVE_KEYWORD_TEXT = 'banana'
+  const KEYWORD_ID = 38353327298089
+  const KEYWORD_TEXT = 'banana'
 
-  describe('createCampaignNegativeKeywords', () => {
+  describe('getCampaignNegativeKeyword', () => {
+    it(`should return a campaign negative keyword ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getCampaignNegativeKeyword(KEYWORD_ID)
+
+      expect(res.campaignId).toBe(MANUAL_CAMPAIGN_ID)
+      expect(res.keywordId).toBe(KEYWORD_ID)
+      expect(res.keywordText).toBe(KEYWORD_TEXT)
+      expect(res.matchType).toBe(CampaignNegativeKeywordMatchTypeEnum.NEGATIVE_EXACT)
+    })
+  })
+
+  describe('getCampaignNegativeKeywordExtended', () => {
+    it(`should return a campaign negative keyword extended ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getCampaignNegativeKeywordExtended(KEYWORD_ID)
+
+      expect(res.campaignId).toBe(MANUAL_CAMPAIGN_ID)
+      expect(res.keywordId).toBe(KEYWORD_ID)
+      expect(res.keywordText).toBe(KEYWORD_TEXT)
+      expect(res.matchType).toBe(CampaignNegativeKeywordMatchTypeEnum.NEGATIVE_EXACT)
+    })
+  })
+
+  describe.skip('createCampaignNegativeKeywords', () => {
     it(`should create a negative keyword for campaign ${POLLY_PASSTHROUGH_TAG}`, async () => {
       const params: CreateCampaignNegativeKeywordsParam[] = [
         {
           campaignId: MANUAL_CAMPAIGN_ID,
 
-          keywordText: NEGATIVE_KEYWORD_TEXT,
+          keywordText: KEYWORD_TEXT,
 
           matchType: CampaignNegativeKeywordMatchTypeEnum.NEGATIVE_EXACT,
 
@@ -35,6 +61,54 @@ describe('SponsoredProductsCampaignNegativeKeywordsOperation', () => {
       const [res] = await operation.createCampaignNegativeKeywords(params)
 
       expect(res.code).toEqual(CampaignNegativeKeywordResponseStatusEnum.SUCCESS)
+    })
+  })
+
+  describe('listCampaignNegativeKeywords', () => {
+    it(`should return an array of campaign negative keywords ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res: CampaignNegativeKeyword[] = await operation.listCampaignNegativeKeywords()
+
+      expect(Array.isArray(res)).toBeTruthy()
+    })
+
+    it(`should return an array of campaign negative keywords when filter additional properties ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: ListCampaignNegativeKeywordsParam = {
+        startIndex: 0,
+        count: 1,
+        matchTypeFilter: CampaignNegativeKeywordMatchTypeEnum.NEGATIVE_EXACT,
+        keywordText: KEYWORD_TEXT,
+        campaignIdFilter: [MANUAL_CAMPAIGN_ID],
+      }
+      const [res] = await operation.listCampaignNegativeKeywords(params)
+
+      expect(res.campaignId).toBe(MANUAL_CAMPAIGN_ID)
+      expect(res.keywordId).toBe(KEYWORD_ID)
+      expect(res.keywordText).toBe(KEYWORD_TEXT)
+      expect(res.matchType).toBe(CampaignNegativeKeywordMatchTypeEnum.NEGATIVE_EXACT)
+    })
+  })
+
+  describe('listCampaignNegativeKeywordsExtended', () => {
+    it(`should return an array of campaign negative keywords extendeds ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res: CampaignNegativeKeywordExtended[] = await operation.listCampaignNegativeKeywordsExtended()
+
+      expect(Array.isArray(res)).toBeTruthy()
+    })
+
+    it(`should return an array of Negative Keyword Extendeds when filter additional properties ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: ListCampaignNegativeKeywordsParam = {
+        startIndex: 0,
+        count: 1,
+        matchTypeFilter: CampaignNegativeKeywordMatchTypeEnum.NEGATIVE_EXACT,
+        keywordText: KEYWORD_TEXT,
+        campaignIdFilter: [MANUAL_CAMPAIGN_ID],
+      }
+      const [res] = await operation.listCampaignNegativeKeywordsExtended(params)
+
+      expect(res.campaignId).toBe(MANUAL_CAMPAIGN_ID)
+      expect(res.keywordId).toBe(KEYWORD_ID)
+      expect(res.keywordText).toBe(KEYWORD_TEXT)
+      expect(res.matchType).toBe(CampaignNegativeKeywordMatchTypeEnum.NEGATIVE_EXACT)
     })
   })
 })
