dd0eb68f49ac92d57e46d030c8f9580f506914ba
Toan Nguyen <nguyentoandev@gmail.com>
2020-01-05 15:43:32 +0700

    feature: implement getAdGroupSuggestedKeywordsExtended operation

--- src/operations/keywords/sp-suggested-keywords-operation.ts
+++ src/operations/keywords/sp-suggested-keywords-operation.ts
@@ -1,13 +1,18 @@
 import { Operation } from '../operation'
-import { Decode } from '../../decorators'
+import { Decode, DecodeArray } from '../../decorators'
 import { AmazonAdTypeURIPrefix } from '../amazon-ad-type-uri-prefix'
-import { AdGroupSuggestedKeywordsResponse, GetAdGroupSuggestedKeywordsParams } from './types'
+import {
+  GetAdGroupSuggestedKeywordsParams,
+  AdGroupSuggestedKeywordsResponse,
+  GetAdGroupSuggestedKeywordsExtendedParams,
+  AdGroupSuggestedKeywordsExtendedResponse,
+} from './types'
 
 export class SponsoredProductsSuggestedKeywordsOperation extends Operation {
   private resourcePostfix = '/suggested/keywords'
   private adGroupResource = `${this.version}/${AmazonAdTypeURIPrefix.SponsoredProducts}/adGroups`
 
-  // TODO: Change adGroupId type to AdGroupId
+  // TODO: Change adGroupId type to AdGroupId type
   @Decode(AdGroupSuggestedKeywordsResponse)
   public getAdGroupSuggestedKeywords(
     adGroupId: number,
@@ -15,6 +20,17 @@ export class SponsoredProductsSuggestedKeywordsOperation extends Operation {
   ) {
     return this.client.get<AdGroupSuggestedKeywordsResponse>(
       this.query(`${this.adGroupResource}/${adGroupId}${this.resourcePostfix}`, params),
+    )
+  }
+
+  // TODO: Change adGroupId type to AdGroupId type
+  @DecodeArray(AdGroupSuggestedKeywordsExtendedResponse)
+  public getAdGroupSuggestedKeywordsExtended(
+    adGroupId: number,
+    params?: GetAdGroupSuggestedKeywordsExtendedParams,
+  ) {
+    return this.client.get<AdGroupSuggestedKeywordsExtendedResponse[]>(
+      this.query(`${this.adGroupResource}/${adGroupId}${this.resourcePostfix}/extended`, params),
     )
   }
 }
--- test/operations/keywords/sp-suggested-keywords-operation.test.ts
+++ test/operations/keywords/sp-suggested-keywords-operation.test.ts
@@ -6,6 +6,8 @@ import { OperationProvider } from '../../../src'
 import {
   GetAdGroupSuggestedKeywordsParams,
   AdGroupStateEnum,
+  GetAdGroupSuggestedKeywordsExtendedParams,
+  SuggestBidsEnum,
 } from '../../../src/operations/keywords/types'
 
 setupPolly()
@@ -17,13 +19,13 @@ describe('SponsoredProductsSuggestedKeywordsOperation', () => {
   const AD_GROUP_ID = 149522344269714
 
   describe('getAdGroupSuggestedKeywords', () => {
-    it(`should suggested keyword data for the specified adGroupId ${POLLY_PASSTHROUGH_TAG}`, async () => {
+    it(`should return suggested keyword data for the specified adGroupId ${POLLY_PASSTHROUGH_TAG}`, async () => {
       const res = await operation.getAdGroupSuggestedKeywords(AD_GROUP_ID)
 
       expect(res.adGroupId).toBe(AD_GROUP_ID)
     })
 
-    it(`should suggested keyword data for the specified adGroupId, adStateFilter ${POLLY_PASSTHROUGH_TAG}`, async () => {
+    it(`should return suggested keyword data for the specified adGroupId, adStateFilter ${POLLY_PASSTHROUGH_TAG}`, async () => {
       const params: GetAdGroupSuggestedKeywordsParams = {
         maxNumSuggestions: 1,
         adStateFilter: [AdGroupStateEnum.ENABLED, AdGroupStateEnum.PAUSED, AdGroupStateEnum.PAUSED],
@@ -31,6 +33,26 @@ describe('SponsoredProductsSuggestedKeywordsOperation', () => {
       const res = await operation.getAdGroupSuggestedKeywords(AD_GROUP_ID, params)
 
       expect(res.adGroupId).toBe(AD_GROUP_ID)
+    })
+  })
+
+  // TODO: response is alway empty. Need check again.
+  describe('getAdGroupSuggestedKeywordsExtended', () => {
+    it(`should return extended suggested keyword data for the specified adGroupId ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getAdGroupSuggestedKeywordsExtended(AD_GROUP_ID)
+
+      expect(Array.isArray(res)).toBeTruthy()
+    })
+
+    it(`should return extended suggested keyword data for the specified adGroupId, suggestBids, adStateFilter ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: GetAdGroupSuggestedKeywordsExtendedParams = {
+        maxNumSuggestions: 1000,
+        suggestBids: SuggestBidsEnum.YES,
+        adStateFilter: [AdGroupStateEnum.ENABLED, AdGroupStateEnum.PAUSED, AdGroupStateEnum.PAUSED],
+      }
+      const res = await operation.getAdGroupSuggestedKeywordsExtended(AD_GROUP_ID, params)
+
+      expect(Array.isArray(res)).toBeTruthy()
     })
   })
 })
