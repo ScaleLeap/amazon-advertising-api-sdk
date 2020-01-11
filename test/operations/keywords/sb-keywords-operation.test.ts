80f1da2768628b2e58e2e56655f8b55cd324c8a8
Toan Nguyen <nguyentoandev@gmail.com>
2020-01-11 15:51:28 +0700

    test: change resource of SponsoredBrandsKeywordsOperation to sb/keywords

---
+++ test/operations/keywords/sb-keywords-operation.test.ts
@@ -0,0 +1,37 @@
+import setupPolly from '../../polly'
+import { SponsoredBrandsKeywordsOperation } from '../../../src/operations/keywords/sb-keywords-operation'
+import { POLLY_PASSTHROUGH_TAG } from '../../constants'
+import { httpClientFactory } from '../../http-client-factory'
+import { OperationProvider } from '../../../src'
+import { CreateSBKeywordParams, KeywordMatchTypeEnum } from '../../../src/operations/keywords/types'
+
+setupPolly()
+
+describe('SponsoredBrandsKeywordsOperation', () => {
+  const client = httpClientFactory()
+  const operationProvider = new OperationProvider(client)
+  const operation = operationProvider.create(SponsoredBrandsKeywordsOperation)
+  const CAMPAIGN_ID = 164069484151709
+  const AD_GROUP_ID = 149522344269714
+  const KEYWORD_TEXT = 'Pear'
+  const BID = 1
+
+  describe('createKeywords', () => {
+    it(`should create a sb keyword ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: CreateSBKeywordParams[] = [
+        {
+          adGroupId: AD_GROUP_ID,
+          campaignId: CAMPAIGN_ID,
+          keywordText: KEYWORD_TEXT,
+          matchType: KeywordMatchTypeEnum.BROAD,
+          bid: BID,
+        },
+      ]
+      const [res] = await operation.createKeywords(params)
+      console.log(res)
+
+      // TODO: replace SUCCESS string with KeywordResponseStatusEnum after merge PR #133
+      expect(res.code).toEqual('SUCCESS')
+    })
+  })
+})
