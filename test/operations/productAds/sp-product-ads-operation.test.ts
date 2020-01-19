2e906551a24904f8e314b0209a9af12c2b2d1372
Toan Nguyen <nguyentoandev@gmail.com>
2020-01-19 15:47:39 +0700

    test: implement getProductAd, getProductAdExtended and updateProductAds operation

--- test/operations/productAds/sp-product-ads-operation.test.ts
+++ test/operations/productAds/sp-product-ads-operation.test.ts
@@ -3,7 +3,11 @@ import { SponsoredProductsProductAdsOperation } from '../../../src/operations/pr
 import { POLLY_PASSTHROUGH_TAG } from '../../constants'
 import { httpClientFactory } from '../../http-client-factory'
 import { OperationProvider } from '../../../src'
-import { CreateProductAdParams, ProductAdStateEnum } from '../../../src/operations/productAds/types'
+import {
+  CreateProductAdParams,
+  ProductAdStateEnum,
+  UpdateProductAdParams,
+} from '../../../src/operations/productAds/types'
 
 setupPolly()
 
@@ -14,8 +18,31 @@ describe('SponsoredProductsProductAdsOperation', () => {
   const CAMPAIGN_ID = 164069484151709
   const AD_GROUP_ID = 149522344269714
   const SKU = 'AB-RED-8675309'
+  const AD_ID = 192944752071234
 
-  describe('createProductAds', () => {
+  describe('getProductAd', () => {
+    it(`should retrieve a product ad by ID ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getProductAd(AD_ID)
+
+      expect(res.campaignId).toEqual(CAMPAIGN_ID)
+      expect(res.adGroupId).toEqual(AD_GROUP_ID)
+      expect(res.sku).toEqual(SKU)
+      expect(res.adId).toEqual(AD_ID)
+    })
+  })
+
+  describe('getProductAdExtended', () => {
+    it(`should retrieve a product ad and its extended fields by ID ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getProductAdExtended(AD_ID)
+
+      expect(res.campaignId).toEqual(CAMPAIGN_ID)
+      expect(res.adGroupId).toEqual(AD_GROUP_ID)
+      expect(res.sku).toEqual(SKU)
+      expect(res.adId).toEqual(AD_ID)
+    })
+  })
+
+  describe.skip('createProductAds', () => {
     it(`should creates one or more product ads ${POLLY_PASSTHROUGH_TAG}`, async () => {
       const params: CreateProductAdParams[] = [
         {
@@ -26,6 +53,20 @@ describe('SponsoredProductsProductAdsOperation', () => {
         },
       ]
       const res = await operation.createProductAds(params)
+
+      expect(Array.isArray(res)).toBeTruthy()
+    })
+  })
+
+  describe('updateProductAds', () => {
+    it(`should updates one or more product ads ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: UpdateProductAdParams[] = [
+        {
+          adId: AD_ID,
+          state: ProductAdStateEnum.PAUSED,
+        },
+      ]
+      const res = await operation.updateProductAds(params)
 
       expect(Array.isArray(res)).toBeTruthy()
     })
