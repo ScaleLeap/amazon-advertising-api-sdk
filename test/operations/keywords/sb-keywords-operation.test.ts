a501594bcf79957b5f14b4d3988f411926bd00a2
Toan Nguyen <nguyentoandev@gmail.com>
2020-02-14 22:47:32 +0700

    test: implement list sb keyword test

--- test/operations/keywords/sb-keywords-operation.test.ts
+++ test/operations/keywords/sb-keywords-operation.test.ts
@@ -1,4 +1,3 @@
-import setupPolly from '../../polly'
 import { SponsoredBrandsKeywordsOperation } from '../../../src/operations/keywords/sb-keywords-operation'
 import { POLLY_PASSTHROUGH_TAG } from '../../constants'
 import { httpClientFactory } from '../../http-client-factory'
@@ -7,9 +6,10 @@ import {
   CreateSBKeywordParams,
   KeywordMatchTypeEnum,
   KeywordResponseStatusEnum,
+  SBKeyword,
+  ListSBKeywordParams,
+  KeywordStateEnum,
 } from '../../../src/operations/keywords/types'
-
-setupPolly()
 
 describe('SponsoredBrandsKeywordsOperation', () => {
   const client = httpClientFactory()
@@ -19,6 +19,29 @@ describe('SponsoredBrandsKeywordsOperation', () => {
   const AD_GROUP_ID = 149522344269714
   const KEYWORD_TEXT = 'Pear'
   const BID = 1
+
+  describe('listKeywords', () => {
+    it(`should return an array of keywords ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res: SBKeyword[] = await operation.listKeywords()
+
+      expect(Array.isArray(res)).toBeTruthy()
+    })
+
+    it(`should return an array of keywords filtered by optional criteria ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: ListSBKeywordParams = {
+        startIndex: 0,
+        count: 1,
+        matchTypeFilter: [KeywordMatchTypeEnum.PHRASE],
+        keywordText: KEYWORD_TEXT,
+        stateFilter: KeywordStateEnum.PAUSED,
+        campaignIdFilter: [CAMPAIGN_ID],
+        adGroupIdFilter: [AD_GROUP_ID],
+      }
+      const res: SBKeyword[] = await operation.listKeywords(params)
+
+      expect(Array.isArray(res)).toBeTruthy()
+    })
+  })
 
   describe.skip('createKeywords', () => {
     it(`should create a sb keyword ${POLLY_PASSTHROUGH_TAG}`, async () => {
