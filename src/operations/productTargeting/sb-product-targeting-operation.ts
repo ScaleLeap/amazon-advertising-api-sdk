37f3782d441087bf7e9780218b81803278c6ed20
Toan Nguyen <nguyentoandev@gmail.com>
2020-02-25 22:44:10 +0700

    feature: implement sb negative product targeting operation

--- src/operations/productTargeting/sb-product-targeting-operation.ts
+++ src/operations/productTargeting/sb-product-targeting-operation.ts
@@ -14,10 +14,21 @@ import {
   SBTargetingClauseResponse,
   SBBatchGetTargetsRequest,
   SBBatchGetTargetsResponse,
+  SBListNegativeTargetsRequest,
+  SBListNegativeTargetsResponse,
+  SBUpdateNegativeTargetsRequest,
+  SBUpdateNegativeTargetsResponse,
+  SBCreateNegativeTargetsResponse,
+  SBCreateNegativeTargetsRequest,
+  SBNegativeTargetingClause,
+  SBNegativeTargetId,
+  SBBatchGetNegativeTargetsResponse,
+  SBBatchGetNegativeTargetsRequest,
 } from './types'
 
 export class SponsoredBrandsProductTargetingOperation extends Operation {
   protected targetResource = `${this.version}/${AmazonAdTypeURIPrefix.SponsoredBrands}/targets`
+  protected negativeTargetResource = `sb/negativeTargets`
 
   @Decode(SBListTargetsResponse)
   public listTargets(params: SBListTargetsRequest) {
@@ -45,7 +56,41 @@ export class SponsoredBrandsProductTargetingOperation extends Operation {
   }
 
   @Decode(SBBatchGetTargetsResponse)
-  public batchGet(params: SBBatchGetTargetsRequest) {
+  public batchGetTargets(params: SBBatchGetTargetsRequest) {
     return this.client.post<SBBatchGetTargetsResponse>(this.targetResource, params)
+  }
+
+  @Decode(SBListNegativeTargetsResponse)
+  public listNegativeTargets(params: SBListNegativeTargetsRequest) {
+    return this.client.post<SBListNegativeTargetsResponse>(this.negativeTargetResource, params)
+  }
+
+  @Decode(SBUpdateNegativeTargetsResponse)
+  public updateNegativeTargets(params: SBUpdateNegativeTargetsRequest) {
+    return this.client.put<SBUpdateNegativeTargetsResponse>(this.negativeTargetResource, params)
+  }
+
+  @Decode(SBCreateNegativeTargetsResponse)
+  public createNegativeTargets(params: SBCreateNegativeTargetsRequest) {
+    return this.client.post<SBCreateNegativeTargetsResponse>(this.negativeTargetResource, params)
+  }
+
+  @Decode(SBNegativeTargetingClause)
+  public getNegativeTarget(negativeTargetId: SBNegativeTargetId) {
+    return this.client.get<SBNegativeTargetingClause>(
+      `${this.negativeTargetResource}/${negativeTargetId}`,
+    )
+  }
+
+  @Decode(SBTargetingClauseResponse)
+  public archiveNegativeTarget(negativeTargetId: SBNegativeTargetId) {
+    return this.client.delete<SBTargetingClauseResponse>(
+      `${this.negativeTargetResource}/${negativeTargetId}`,
+    )
+  }
+
+  @Decode(SBBatchGetNegativeTargetsResponse)
+  public batchGetNegativeTargets(params: SBBatchGetNegativeTargetsRequest) {
+    return this.client.post<SBBatchGetNegativeTargetsResponse>(this.negativeTargetResource, params)
   }
 }
--- test/operations/productTargeting/sb-product-targeting-operation.test.ts
+++ test/operations/productTargeting/sb-product-targeting-operation.test.ts
@@ -11,6 +11,11 @@ import {
   SBUpdateTargetsRequest,
   SBExpressionStateEnum,
   SBBatchGetTargetsRequest,
+  SBListNegativeTargetsRequest,
+  SBUpdateNegativeTargetsRequest,
+  SBCreateNegativeTargetsRequest,
+  SBNegativeExpressionTypeEnum,
+  SBBatchGetNegativeTargetsRequest,
 } from '../../../src/operations/productTargeting/types'
 
 /**
@@ -24,6 +29,7 @@ describe.skip('SponsoredBrandsProductTargetingOperation', () => {
   const CAMPAIGN_ID = 164069484151709
   const AD_GROUP_ID = 202694745498469
   const TARGET_ID = 256247845256441
+  const NEGATIVE_TARGET_ID = 256247845256441
   const BID = 1
 
   describe('listTargets', () => {
@@ -87,7 +93,8 @@ describe.skip('SponsoredBrandsProductTargetingOperation', () => {
       }
       const res = await operation.createTargets(params)
 
-      expect(Array.isArray(res)).toBeTruthy()
+      expect(Array.isArray(res.createTargetErrorResults)).toBeTruthy()
+      expect(Array.isArray(res.createTargetSuccessResults)).toBeTruthy()
     })
   })
 
@@ -114,10 +121,105 @@ describe.skip('SponsoredBrandsProductTargetingOperation', () => {
       const params: SBBatchGetTargetsRequest = {
         targetIds: [TARGET_ID],
       }
-      const res = await operation.batchGet(params)
+      const res = await operation.batchGetTargets(params)
 
       expect(Array.isArray(res.batchGetTargetErrorResults)).toBeTruthy()
       expect(Array.isArray(res.batchGetTargetSuccessResults)).toBeTruthy()
+    })
+  })
+
+  describe('listNegativeTargets', () => {
+    it(`should return a list of sb negative targets filtered by specified criteria ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: SBListNegativeTargetsRequest = {
+        nextToken: 'string',
+        maxResults: 1,
+        filters: [
+          {
+            filterType: SBFilterTypeEnum.STATE,
+            values: [SBTargetStateEnum.PAUSED],
+          },
+          {
+            filterType: SBFilterTypeEnum.CAMPAIGN_ID,
+            values: [CAMPAIGN_ID],
+          },
+          {
+            filterType: SBFilterTypeEnum.AD_GROUP_ID,
+            values: [AD_GROUP_ID],
+          },
+        ],
+      }
+      const res = await operation.listNegativeTargets(params)
+
+      expect(res).toHaveProperty('nextToken')
+    })
+  })
+
+  describe('updateNegativeTargets', () => {
+    it(`should update one or more sb negative targets ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: SBUpdateNegativeTargetsRequest = {
+        negativeTargets: [
+          {
+            targetId: NEGATIVE_TARGET_ID,
+            adGroupId: AD_GROUP_ID,
+            state: SBExpressionStateEnum.ARCHIVED,
+          },
+        ],
+      }
+      const res = await operation.updateNegativeTargets(params)
+
+      expect(Array.isArray(res.updateTargetErrorResults)).toBeTruthy()
+      expect(Array.isArray(res.updateTargetSuccessResults)).toBeTruthy()
+    })
+  })
+
+  describe('createNegativeTargets', () => {
+    it(`should create one or more sb negative targets ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: SBCreateNegativeTargetsRequest = {
+        negativeTargets: [
+          {
+            adGroupId: AD_GROUP_ID,
+            campaignId: CAMPAIGN_ID,
+            expressions: {
+              type: SBNegativeExpressionTypeEnum.ASIN_BRAND_SAME_AS,
+              value: 'Apple',
+            },
+          },
+        ],
+      }
+      const res = await operation.createNegativeTargets(params)
+
+      expect(Array.isArray(res.createTargetErrorResults)).toBeTruthy()
+      expect(Array.isArray(res.createTargetSuccessResults)).toBeTruthy()
+    })
+  })
+
+  describe('getNegativeTarget', () => {
+    it(`should return a sb negative target specified by identifier ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.getNegativeTarget(NEGATIVE_TARGET_ID)
+
+      expect(res.campaignId).toEqual(CAMPAIGN_ID)
+      expect(res.adGroupId).toEqual(AD_GROUP_ID)
+      expect(res.targetId).toEqual(NEGATIVE_TARGET_ID)
+    })
+  })
+
+  describe('archiveNegativeTarget', () => {
+    it(`should archive a sb negative target specified by identifier ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const res = await operation.archiveNegativeTarget(NEGATIVE_TARGET_ID)
+
+      expect(res).toHaveProperty('targetId')
+    })
+  })
+
+  describe('archiveNegativeTarget', () => {
+    it(`should archive a sb negative target specified by identifier ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: SBBatchGetNegativeTargetsRequest = {
+        targetIds: [NEGATIVE_TARGET_ID],
+      }
+      const res = await operation.batchGetNegativeTargets(params)
+
+      expect(Array.isArray(res.batchGetNegativeTargetErrorResults)).toBeTruthy()
+      expect(Array.isArray(res.batchGetNegativeTargetSuccessResults)).toBeTruthy()
     })
   })
 })
