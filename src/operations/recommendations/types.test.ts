705f3372dc2b783d5f7b3e8d9d7a50dbcbbd97dc
Toan Nguyen <nguyentoandev@gmail.com>
2020-02-28 22:21:30 +0700

    feature: implement sb bid recommendations request and response type

--- src/operations/recommendations/types.test.ts
+++ src/operations/recommendations/types.test.ts
@@ -74,3 +74,90 @@ describe('SBCategoryRecommendationsResponse', () => {
     expect(isRight(res)).toBeTruthy()
   })
 })
+
+describe('SBBidsRecommendationRequest', () => {
+  it('should pass', () => {
+    const res = t.SBBidsRecommendationRequest.decode({
+      campaignId: 0,
+      targets: [
+        {
+          type: 'asinCategorySameAs',
+          value: 'string',
+        },
+      ],
+      keywords: [
+        {
+          matchType: 'broad',
+          keywordText: 'string',
+        },
+      ],
+    })
+
+    expect(isRight(res)).toBeTruthy()
+  })
+})
+
+describe('SBBidsRecommendationResponse', () => {
+  it('should pass', () => {
+    const res = t.SBBidsRecommendationResponse.decode({
+      keywordsBidsRecommendationSuccessResults: [
+        {
+          recommendationId: 'string',
+          recommendedBid: {
+            rangeEnd: 0,
+            rangeStart: 0,
+            recommended: 0,
+          },
+          keyword: {
+            keywordText: 'string',
+            matchType: 'broad',
+          },
+          keywordIndex: 0,
+        },
+      ],
+      keywordsBidsRecommendationErrorResults: [
+        {
+          code: 'string',
+          details: 'string',
+          keyword: {
+            type: 'broad',
+            value: 'string',
+          },
+          keywordIndex: 0,
+        },
+      ],
+      targetsBidsRecommendationSuccessResults: [
+        {
+          recommendationId: 'string',
+          recommendedBid: {
+            rangeEnd: 0,
+            rangeStart: 0,
+            recommended: 0,
+          },
+          targets: [
+            {
+              type: 'asinCategorySameAs',
+              value: 'string',
+            },
+          ],
+          targetsIndex: 0,
+        },
+      ],
+      targetsBidsRecommendationErrorResults: [
+        {
+          code: 'string',
+          details: 'string',
+          targets: [
+            {
+              type: 'asinCategorySameAs',
+              value: 'string',
+            },
+          ],
+          targetsIndex: 0,
+        },
+      ],
+    })
+
+    expect(isRight(res)).toBeTruthy()
+  })
+})
--- src/operations/recommendations/types.ts
+++ src/operations/recommendations/types.ts
@@ -1,6 +1,8 @@
 import * as t from 'io-ts'
 import { createEnumType } from '../commons/types'
-import { BrandId } from '../productTargeting/types'
+import { BrandId, SBExpressions } from '../productTargeting/types'
+import { CampaignId } from '../campaigns/types'
+import { KeywordMatchType } from '../keywords/types'
 
 export enum SBProductRecommendationFilterTypeEnum {
   ASINS = 'ASINS',
@@ -120,3 +122,137 @@ export const SBBrandRecommendationsResponse = t.strict({
   brandRecommendationResults: t.array(SBBrandResponse),
 })
 export type SBBrandRecommendationsResponse = t.TypeOf<typeof SBBrandRecommendationsResponse>
+
+/**
+ * Bid recommendations types
+ */
+
+const SBBidRecommendationKeyword = t.strict({
+  /**
+   * The match type.
+   * For more information, see match types in the Amazon Advertising support center.
+   */
+  matchType: KeywordMatchType,
+
+  /**
+   * The text of the keyword. Maximum of 10 words.
+   */
+  keywordText: t.string,
+})
+
+const SBRecommendedBid = t.strict({
+  rangeStart: t.number,
+
+  rangeEnd: t.number,
+
+  recommended: t.number,
+})
+
+export enum SBKeywordExpressionTypeEnum {
+  BROAD = 'broad',
+  EXACT = 'exact',
+  PHRASE = 'phrase',
+}
+export const SBKeywordExpressionType = createEnumType<SBKeywordExpressionTypeEnum>(
+  SBKeywordExpressionTypeEnum,
+)
+
+export const RecommendationId = t.string
+export type RecommendationId = t.TypeOf<typeof RecommendationId>
+
+export const SBBidsRecommendationRequest = t.strict({
+  /**
+   * The identifier of the campaign for which bid recommendations are created.
+   */
+  campaignId: CampaignId,
+
+  targets: SBExpressions,
+
+  keywords: t.array(SBBidRecommendationKeyword),
+})
+export type SBBidsRecommendationRequest = t.TypeOf<typeof SBBidsRecommendationRequest>
+
+export const SBBidsRecommendationResponse = t.strict({
+  /**
+   * Lists the bid recommendations for the keywords specified in the request.
+   */
+  keywordsBidsRecommendationSuccessResults: t.array(
+    t.strict({
+      /**
+       * The identifier of the keyword bid recommendation.
+       */
+      recommendationId: RecommendationId,
+
+      recommendedBid: SBRecommendedBid,
+
+      keyword: SBBidRecommendationKeyword,
+
+      keywordIndex: t.number,
+    }),
+  ),
+
+  /**
+   * Lists errors that occured during creation of keyword bid recommendations.
+   */
+  keywordsBidsRecommendationErrorResults: t.array(
+    t.strict({
+      code: t.string,
+
+      details: t.string,
+
+      keyword: t.strict({
+        /**
+         * The text of the keyword. Maximum of 10 words.
+         */
+        value: t.string,
+
+        /**
+         * The match type. For more information, see match types in the Amazon Advertising support center.
+         */
+        type: SBKeywordExpressionType,
+      }),
+
+      keywordIndex: t.number,
+    }),
+  ),
+
+  /**
+   * Lists the bid recommendations for the keywords or targets specified in the request.
+   */
+  targetsBidsRecommendationSuccessResults: t.array(
+    t.strict({
+      /**
+       * The identifier of the target bid recommendation.
+       */
+      recommendationId: RecommendationId,
+
+      recommendedBid: SBRecommendedBid,
+
+      targets: SBExpressions,
+
+      /**
+       * Correlates the keyword to the keyword array index specified in the request. Zero-based.
+       */
+      targetsIndex: t.number,
+    }),
+  ),
+
+  /**
+   * Lists errors that occured during creation of target bid recommendations.
+   */
+  targetsBidsRecommendationErrorResults: t.array(
+    t.strict({
+      code: t.string,
+
+      details: t.string,
+
+      targets: SBExpressions,
+
+      /**
+       * Correlates the keyword to the keyword array index specified in the request. Zero-based.
+       */
+      targetsIndex: t.number,
+    }),
+  ),
+})
+export type SBBidsRecommendationResponse = t.TypeOf<typeof SBBidsRecommendationResponse>
