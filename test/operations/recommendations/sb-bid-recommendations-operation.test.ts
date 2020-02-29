addafa39b9398b9732158a80d7cc391f8c143aad
Toan Nguyen <nguyentoandev@gmail.com>
2020-02-29 13:35:18 +0700

    feature: implement sb bid recommendations operation and test

---
+++ test/operations/recommendations/sb-bid-recommendations-operation.test.ts
@@ -0,0 +1,43 @@
+import { OperationProvider } from '../../../src/operations/operation-provider'
+import { SponsoredBrandsBidRecommendationsOperation } from '../../../src/operations/recommendations/sb-bid-recommendations-operation'
+import { httpClientFactory } from '../../http-client-factory'
+import { POLLY_PASSTHROUGH_TAG } from '../../constants'
+import { SBBidsRecommendationRequest } from '../../../src/operations/recommendations/types'
+import { ProductPredicateTypeEnum } from '../../../src/operations/productTargeting/types'
+import { KeywordMatchTypeEnum } from '../../../src/operations/keywords/types'
+
+/**
+ * TODO: Need check again on Production API. Sandbox API returns an error:
+ * Could not find resource for full path: https://advertising-api-test.amazon.com/v1/hsa/recommendations/bids
+ */
+describe('SponsoredBrandsBidRecommendationsOperation', () => {
+  const client = httpClientFactory()
+  const operationProvider = new OperationProvider(client)
+  const operation = operationProvider.create(SponsoredBrandsBidRecommendationsOperation)
+
+  describe('getBidRecommendations', () => {
+    it(`should return a list of bid recommendation for keywords or products ${POLLY_PASSTHROUGH_TAG}`, async () => {
+      const params: SBBidsRecommendationRequest = {
+        campaignId: 0,
+        targets: [
+          {
+            type: ProductPredicateTypeEnum.ASIN_CATEGORY_SAME_AS,
+            value: 'string',
+          },
+        ],
+        keywords: [
+          {
+            matchType: KeywordMatchTypeEnum.BROAD,
+            keywordText: 'string',
+          },
+        ],
+      }
+      const res = await operation.getBidRecommendations(params)
+
+      expect(res).toHaveProperty('keywordsBidsRecommendationSuccessResults')
+      expect(res).toHaveProperty('keywordsBidsRecommendationErrorResults')
+      expect(res).toHaveProperty('targetsBidsRecommendationSuccessResults')
+      expect(res).toHaveProperty('targetsBidsRecommendationErrorResults')
+    })
+  })
+})
