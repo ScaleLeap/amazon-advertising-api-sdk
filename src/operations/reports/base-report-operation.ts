d518dc70ea26aa41aa6396833dbae9ad276c346a
Toan Nguyen <nguyentoandev@gmail.com>
2020-03-09 22:43:23 +0700

    chore: Add jsdoc doc blocks to report and snapshot operation

--- src/operations/reports/base-report-operation.ts
+++ src/operations/reports/base-report-operation.ts
@@ -3,11 +3,26 @@ import { Operation } from '../operation'
 import { ReportResponse, ReportId } from './report-response'
 
 export class BaseReportOperation extends Operation {
+  /**
+   * Retrieve status, metadata and location of a previously requested performance report.
+   *
+   * @param {ReportId} reportId
+   * @returns
+   * @memberof BaseReportOperation
+   */
   @Decode(ReportResponse)
   public getReport(reportId: ReportId) {
     return this.client.get<ReportResponse>(`${this.version}/reports/${reportId}`)
   }
 
+  /**
+   * Downloads the report corresponding to the reportId specified
+   *
+   * @template T
+   * @param {ReportId} reportId
+   * @returns {(Promise<Partial<Record<T, 'number' | 'string'>>[]>)}
+   * @memberof BaseReportOperation
+   */
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   public async downloadReport<T extends string>(
     reportId: ReportId,
--- src/operations/reports/sponsored-brands/sponsored-brands-report-operation.ts
+++ src/operations/reports/sponsored-brands/sponsored-brands-report-operation.ts
@@ -16,6 +16,14 @@ export class SponsoredBrandsReportOperation<
 > extends BaseReportOperation {
   private type = 'hsa'
 
+  /**
+   * Request the creation of a performance report for all entities of a single type which have performance data to report.
+   * Record types can be: campaigns, adGroups, and keywords
+   *
+   * @param {ReportParams} params
+   * @returns
+   * @memberof SponsoredBrandsReportOperation
+   */
   @Decode(ReportResponse)
   public requestReport(params: ReportParams) {
     return this.client.post<ReportResponse>(
