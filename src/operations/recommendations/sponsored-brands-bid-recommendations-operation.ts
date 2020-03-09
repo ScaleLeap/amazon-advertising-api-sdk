9e6866ea9986a0fbe70fbf8317b479e94c04aecd
Toan Nguyen <nguyentoandev@gmail.com>
2020-03-09 22:42:43 +0700

    chore: Add jsdoc doc blocks to recommendations operation

--- src/operations/recommendations/sponsored-brands-bid-recommendations-operation.ts
+++ src/operations/recommendations/sponsored-brands-bid-recommendations-operation.ts
@@ -9,6 +9,13 @@ import {
 export class SponsoredBrandsBidRecommendationsOperation extends Operation {
   protected resource = `${this.version}/${AmazonAdTypeURIPrefix.SponsoredBrands}/recommendations/bids`
 
+  /**
+   * Get a list of bid recommendation objects for a specified list of keywords or products.
+   *
+   * @param {SponsoredBrandsBidsRecommendationRequest} params
+   * @returns
+   * @memberof SponsoredBrandsBidRecommendationsOperation
+   */
   @Decode(SponsoredBrandsBidsRecommendationResponse)
   public getBidRecommendations(params: SponsoredBrandsBidsRecommendationRequest) {
     return this.client.post<SponsoredBrandsBidsRecommendationResponse>(this.resource, params)
