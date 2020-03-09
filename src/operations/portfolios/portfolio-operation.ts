cb5f69c1ae8675172c3999c2d3b40f9ab3a8d533
Toan Nguyen <nguyentoandev@gmail.com>
2020-03-09 22:38:26 +0700

    chore: Add jsdoc doc blocks to portfolio operation

--- src/operations/portfolios/portfolio-operation.ts
+++ src/operations/portfolios/portfolio-operation.ts
@@ -13,31 +13,73 @@ import { Decode, DecodeArray } from '../../decorators'
 export class PortfolioOperation extends Operation {
   protected resource = `${this.version}/portfolios`
 
+  /**
+   * Retrieve a list of up to 100 portfolios using the specified filters.
+   *
+   * @param {ListPortfoliosParams} [params]
+   * @returns
+   * @memberof PortfolioOperation
+   */
   @DecodeArray(Portfolio)
   public listPortfolios(params?: ListPortfoliosParams) {
     return this.client.get<Portfolio[]>(this.query(this.resource, params))
   }
 
+  /**
+   * Retrieve a list of up to 100 portfolios with additional properties using the specified filters.
+   *
+   * @param {ListPortfoliosParams} [params]
+   * @returns
+   * @memberof PortfolioOperation
+   */
   @DecodeArray(PortfolioExtended)
   public listPortfoliosEx(params?: ListPortfoliosParams) {
     return this.client.get<PortfolioExtended[]>(this.query(`${this.resource}/extended`, params))
   }
 
+  /**
+   * Retrieve a portfolio by ID.
+   *
+   * @param {PortfolioId} portfolioId
+   * @returns
+   * @memberof PortfolioOperation
+   */
   @Decode(Portfolio)
   public getPortfolio(portfolioId: PortfolioId) {
     return this.client.get<Portfolio>(`${this.resource}/${portfolioId}`)
   }
 
+  /**
+   * Retrieve a portfolio along with additional properties by ID.
+   *
+   * @param {PortfolioId} portfolioId
+   * @returns
+   * @memberof PortfolioOperation
+   */
   @Decode(PortfolioExtended)
   public getPortfolioEx(portfolioId: PortfolioId) {
     return this.client.get<PortfolioExtended>(`${this.resource}/extended/${portfolioId}`)
   }
 
+  /**
+   * Create one or more portfolios. Maximum number of portfolios per account is 100.
+   *
+   * @param {CreatePortfoliosParams[]} portfolios
+   * @returns
+   * @memberof PortfolioOperation
+   */
   @DecodeArray(PortfolioResponse)
   public createPortfolios(portfolios: CreatePortfoliosParams[]) {
     return this.client.post<PortfolioResponse[]>(this.resource, portfolios)
   }
 
+  /**
+   * Update one or more portfolios.
+   *
+   * @param {UpdatePortfoliosParams[]} portfolios
+   * @returns
+   * @memberof PortfolioOperation
+   */
   @DecodeArray(PortfolioResponse)
   public updatePortfolios(portfolios: UpdatePortfoliosParams[]) {
     return this.client.put<PortfolioResponse[]>(this.resource, portfolios)
