fcbc42136679bd51a090c7e38b1459bcd24542e5
Toan Nguyen <nguyentoandev@gmail.com>
2020-03-09 22:42:03 +0700

    chore: Add jsdoc doc blocks to profile operation

--- src/operations/profiles/profile-operation.ts
+++ src/operations/profiles/profile-operation.ts
@@ -13,21 +13,49 @@ import { CountryCodeEnum, CountryCodeType } from '../commons/types'
 export class ProfileOperation extends Operation {
   protected resource = 'profiles'
 
+  /**
+   * Retrieves one or more profiles associated with the authorization passed in the request header.
+   *
+   * @returns
+   * @memberof ProfileOperation
+   */
   @DecodeArray(Profile)
   public listProfiles() {
     return this.client.get<Profile[]>(`${this.version}/${this.resource}`)
   }
 
+  /**
+   * Retrieves a single profile specified by identifier.
+   *
+   * @param {ProfileId} profileId
+   * @returns
+   * @memberof ProfileOperation
+   */
   @Decode(Profile)
   public getProfile(profileId: ProfileId) {
     return this.client.get<Profile>(`${this.version}/${this.resource}/${profileId}`)
   }
 
+  /**
+   * Updates one or more profiles.
+   *
+   * @param {Partial<Profile>[]} profiles
+   * @returns
+   * @memberof ProfileOperation
+   */
   @DecodeArray(ProfileResponse)
   public updateProfiles(profiles: Partial<Profile>[]) {
     return this.client.put<ProfileResponse[]>(`${this.version}/${this.resource}`, profiles)
   }
 
+  /**
+   * Registers a profile in the sandbox environment.
+   * If this call is made to a production endpoint an error is returned.
+   *
+   * @param {CountryCodeType} [countryCode=CountryCodeEnum.US]
+   * @returns
+   * @memberof ProfileOperation
+   */
   @Decode(RegisterProfileResponse)
   public registerProfile(countryCode: CountryCodeType = CountryCodeEnum.US) {
     return this.client.put<RegisterProfileResponse>(`${this.version}/${this.resource}/register`, {
@@ -35,6 +63,13 @@ export class ProfileOperation extends Operation {
     })
   }
 
+  /**
+   * Registers a brand in the sandbox environment. If this call is made to a production endpoint an error is returned.
+   *
+   * @param {RegisterBrand} registerBrand
+   * @returns
+   * @memberof ProfileOperation
+   */
   @Decode(ProfileRegistrationResponse)
   public registerBrand(registerBrand: RegisterBrand) {
     return this.client.put<ProfileRegistrationResponse>(
