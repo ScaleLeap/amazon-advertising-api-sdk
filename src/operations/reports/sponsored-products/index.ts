3f1f18970eee618ea62ae915f4228bfcd96c784b
Toan Nguyen <nguyentoandev@gmail.com>
2020-03-28 22:20:35 +0700

    chore: add export point for report types

---
+++ src/operations/reports/sponsored-products/index.ts
@@ -0,0 +1,6 @@
+export * from './sponsored-products-ad-group-report-params'
+export * from './sponsored-products-asins-report-params'
+export * from './sponsored-products-campaign-report-params'
+export * from './sponsored-products-keyword-report-params'
+export * from './sponsored-products-product-ads-report-params'
+export * from './sponsored-products-targets-report-params'
