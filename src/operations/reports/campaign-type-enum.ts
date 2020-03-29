d69fa76920dee05d15e684ae8b575616aff48e3a
Toan Nguyen <nguyentoandev@gmail.com>
2020-03-29 17:03:02 +0700

    feat: remove duplicate CampaignTypeEnum

--- src/operations/reports/campaign-type-enum.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Copyright (C) 2019, Scale Leap
- */
-
-export enum CampaignTypeEnum {
-  SPONSORED_PRODUCTS = 'sponsoredProducts',
-}
--- src/operations/reports/index.ts
+++ src/operations/reports/index.ts
@@ -1,7 +1,6 @@
 export * from './metrics/index'
 export * from './sponsored-brands/index'
 export * from './sponsored-products/index'
-export * from './campaign-type-enum'
 export * from './report-response'
 export * from './report-segments-enum'
 export * from './report-types-enum'
