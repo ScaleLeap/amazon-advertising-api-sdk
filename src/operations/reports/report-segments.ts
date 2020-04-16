359c1c36e231e6919f0cd878c82fdf5a5572837d
Toan Nguyen <nguyentoandev@gmail.com>
2020-04-16 22:25:44 +0700

    feat: change ReportSegments to use union string

--- src/operations/reports/report-segments.ts
+++ src/operations/reports/report-segments.ts
@@ -1,13 +1,16 @@
-export enum ReportSegmentsEnum {
+import * as t from 'io-ts'
+
+export const ReportSegments = t.union([
   /**
    * The optional dimension on which to segment a keyword report.
    * This is also referred to as the search terms report.
    */
-  QUERY = 'query',
+  t.literal('query'),
 
   /**
    * The optional dimension on which to segment a campaigns report.
    * Placement refers to the location on a page where your ad appears.
    */
-  PLACEMENT = 'placement',
-}
+  t.literal('placement'),
+])
+export type ReportSegments = t.TypeOf<typeof ReportSegments>
