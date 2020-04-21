a65a9f06b5bb3878732341b11f750a16133c0ae1
Toan Nguyen <nguyentoandev@gmail.com>
2020-04-21 22:40:15 +0700

    feat: implement get ad group, get keyword, create keyword bid recommendation methods

---
+++ src/operations/bidding/sponsored-products-bid-recommendation-operation.ts
@@ -0,0 +1,58 @@
+import { Operation } from '../operation'
+import { Decode } from '../../decorators'
+import { AmazonAdTypeURIPrefix } from '../amazon-ad-type-uri-prefix'
+import { AdGroupId } from '../ad-groups/types'
+import {
+  AdGroupBidRecommendationsResponse,
+  KeywordBidRecommendationsResponse,
+  KeywordBidRecommendationsData,
+  BidRecommendationsResponse,
+} from './types'
+import { KeywordId } from '../keywords/types'
+
+export class SponsoredProductsBidRecommendationOperation extends Operation {
+  protected resource = `${this.version}/${AmazonAdTypeURIPrefix.SponsoredProducts}/`
+
+  /**
+   * Request bid recommendations for specified ad group.
+   *
+   * @param {AdGroupId} id
+   * @returns AdGroupBidRecommendationsResponse
+   * @memberof SponsoredProductsBidRecommendationOperation
+   */
+  @Decode(AdGroupBidRecommendationsResponse)
+  public getAdGroupBidRecommendations(id: AdGroupId) {
+    return this.client.get<AdGroupBidRecommendationsResponse>(
+      `${this.resource}/adGroups/${id}/bidRecommendations`,
+    )
+  }
+
+  /**
+   * Request bid recommendations for specified keyword.
+   *
+   * @param {KeywordId} id
+   * @returns KeywordBidRecommendationsResponse
+   * @memberof SponsoredProductsBidRecommendationOperation
+   */
+  @Decode(KeywordBidRecommendationsResponse)
+  public getKeywordBidRecommendations(id: KeywordId) {
+    return this.client.get<KeywordBidRecommendationsResponse>(
+      `${this.resource}/keywords/${id}/bidRecommendations`,
+    )
+  }
+
+  /**
+   * Request bid recommendations for a list of up to 100 keywords.
+   *
+   * @param {KeywordBidRecommendationsData} params
+   * @returns BidRecommendationsResponse
+   * @memberof SponsoredProductsBidRecommendationOperation
+   */
+  @Decode(BidRecommendationsResponse)
+  public createKeywordBidRecommendations(params: KeywordBidRecommendationsData) {
+    return this.client.post<BidRecommendationsResponse>(
+      `${this.resource}/keywords/bidRecommendations`,
+      params,
+    )
+  }
+}
