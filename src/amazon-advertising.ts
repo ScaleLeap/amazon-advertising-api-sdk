307f92b5aad355b10e4ea97d09ce7dc6a362c103
Toan Nguyen <nguyentoandev@gmail.com>
2020-04-26 18:05:19 +0700

    feat: update operations into main entry point class

--- src/amazon-advertising.ts
+++ src/amazon-advertising.ts
@@ -3,7 +3,33 @@ import { LazyGetter } from 'lazy-get-decorator'
 import { HttpClientAuth } from './http-client'
 import { HttpClient } from './http-client'
 import { OperationProvider } from './operations/operation-provider'
-import { SponsoredProductsCampaignOperation } from './operations/campaigns/sponsored-products-campaign-operation'
+import {
+  SponsoredBrandsAdGroupOperation,
+  SponsoredProductsAdGroupOperation,
+  SponsoredProductsBidRecommendationOperation,
+  SponsoredBrandsCampaignOperation,
+  SponsoredProductsCampaignOperation,
+  SponsoredBrandsDraftsOperation,
+  SponsoredBrandsKeywordRecommendationsOperation,
+  SponsoredBrandsKeywordsOperation,
+  SponsoredBrandsNegativeKeywordsOperation,
+  SponsoredProductsAdGroupKeywordsOperation,
+  SponsoredProductsAdGroupNegativeKeywordsOperation,
+  SponsoredProductsCampaignNegativeKeywordsOperation,
+  SponsoredProductsSuggestedKeywordsOperation,
+  PortfolioOperation,
+  SponsoredProductsProductAdsOperation,
+  SponsoredBrandsProductTargetingOperation,
+  SponsoredProductsProductTargetingOperation,
+  ProfileOperation,
+  SponsoredBrandsBidRecommendationsOperation,
+  SponsoredBrandsTargetingRecommendationsOperation,
+  SponsoredBrandsReportOperation,
+  SponsoredProductsReportOperation,
+  SponsoredBrandsSnapshotOperation,
+  SponsoredProductsSnapshotOperation,
+  SponsoredBrandsStoresInfoOperation,
+} from './index'
 
 export class AmazonAdvertising {
   private operationProvider: OperationProvider
@@ -20,7 +46,127 @@ export class AmazonAdvertising {
   }
 
   @LazyGetter()
+  get sponsoredBrandsAdGroup() {
+    return this.operationProvider.create(SponsoredBrandsAdGroupOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsAdGroup() {
+    return this.operationProvider.create(SponsoredProductsAdGroupOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsBidRecommendation() {
+    return this.operationProvider.create(SponsoredProductsBidRecommendationOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsCampaign() {
+    return this.operationProvider.create(SponsoredBrandsCampaignOperation)
+  }
+
+  @LazyGetter()
   get sponsoredProductsCampaign() {
     return this.operationProvider.create(SponsoredProductsCampaignOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsDrafts() {
+    return this.operationProvider.create(SponsoredBrandsDraftsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsKeywordRecommendations() {
+    return this.operationProvider.create(SponsoredBrandsKeywordRecommendationsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsKeywords() {
+    return this.operationProvider.create(SponsoredBrandsKeywordsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsNegativeKeywords() {
+    return this.operationProvider.create(SponsoredBrandsNegativeKeywordsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsAdGroupKeywords() {
+    return this.operationProvider.create(SponsoredProductsAdGroupKeywordsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsAdGroupNegativeKeywords() {
+    return this.operationProvider.create(SponsoredProductsAdGroupNegativeKeywordsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsCampaignNegativeKeywords() {
+    return this.operationProvider.create(SponsoredProductsCampaignNegativeKeywordsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsSuggestedKeywords() {
+    return this.operationProvider.create(SponsoredProductsSuggestedKeywordsOperation)
+  }
+
+  @LazyGetter()
+  get portfolio() {
+    return this.operationProvider.create(PortfolioOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsProductAds() {
+    return this.operationProvider.create(SponsoredProductsProductAdsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsProductTargeting() {
+    return this.operationProvider.create(SponsoredBrandsProductTargetingOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsProductTargeting() {
+    return this.operationProvider.create(SponsoredProductsProductTargetingOperation)
+  }
+
+  @LazyGetter()
+  get profile() {
+    return this.operationProvider.create(ProfileOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsBidRecommendations() {
+    return this.operationProvider.create(SponsoredBrandsBidRecommendationsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsTargetingRecommendations() {
+    return this.operationProvider.create(SponsoredBrandsTargetingRecommendationsOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsReport() {
+    return this.operationProvider.create(SponsoredBrandsReportOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsReport() {
+    return this.operationProvider.create(SponsoredProductsReportOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsSnapshot() {
+    return this.operationProvider.create(SponsoredBrandsSnapshotOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredProductsSnapshot() {
+    return this.operationProvider.create(SponsoredProductsSnapshotOperation)
+  }
+
+  @LazyGetter()
+  get sponsoredBrandsStoresInfo() {
+    return this.operationProvider.create(SponsoredBrandsStoresInfoOperation)
   }
 }
