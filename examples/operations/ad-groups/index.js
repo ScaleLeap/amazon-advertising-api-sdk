81e7f8b0f887374baa9c4f77902063ed2a118352
Toan Nguyen <nguyentoandev@gmail.com>
2020-05-03 23:04:13 +0700

    chore: create base example for sb ad group operation

---
+++ examples/operations/ad-groups/index.js
@@ -0,0 +1,13 @@
+import { amazonMarketplaces } from '@scaleleap/amazon-marketplaces'
+import { AmazonAdvertising } from '@scaleleap/amazon-advertising-api-sdk'
+
+const auth = {
+  accessToken: 'Atza|IQEBLjAsAhRmHjNgHpi0U...',
+  clientId: 'amzn1.application-oa2-client.a8358a60...',
+  scope: 1234567890,
+}
+const amazonAdvertising = new AmazonAdvertising(amazonMarketplaces.US, auth)
+const sponsoredBrandsAdGroupOperation = amazonAdvertising.sponsoredBrandsAdGroup
+
+const sponsoredBrandsAdGroup = await sponsoredBrandsAdGroupOperation.getAdGroup('1111111111')
+const sponsoredBrandsAdGroups = await sponsoredBrandsAdGroupOperation.listAdGroups()
