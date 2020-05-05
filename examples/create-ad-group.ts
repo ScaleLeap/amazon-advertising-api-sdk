08589c4d7262f8076d477591d1f826582cb0f604
Toan Nguyen <nguyentoandev@gmail.com>
2020-05-05 22:01:52 +0700

    chore: create sp ad group operation example

--- examples/create-ad-group.ts
+++ examples/create-ad-group.ts
@@ -1,5 +1,8 @@
 import { amazonMarketplaces } from '@scaleleap/amazon-marketplaces'
-import { AmazonAdvertising } from '@scaleleap/amazon-advertising-api-sdk'
+import { 
+  AmazonAdvertising,
+  AdGroup,
+} from '@scaleleap/amazon-advertising-api-sdk'
 
 const auth = {
   accessToken: 'Atza|IQEBLjAsAhRmHjNgHpi0U...',
@@ -10,8 +13,36 @@ const amazonAdvertising = new AmazonAdvertising(amazonMarketplaces.US, auth)
 const sponsoredBrandsAdGroupOperation = amazonAdvertising.sponsoredBrandsAdGroup
 const sponsoredProductsAdGroupOperation = amazonAdvertising.sponsoredProductsAdGroup
 
-const sponsoredBrandsAdGroup = await sponsoredBrandsAdGroupOperation.getAdGroup(123)
-const sponsoredBrandsAdGroups = await sponsoredBrandsAdGroupOperation.listAdGroups()
+// Gets an array of ad groups associated with the client identifier passed in the authorization header, filtered by specified criteria.
+sponsoredBrandsAdGroupOperation.getAdGroup(123)
 
-const sponsoredProductsAdGroup = await sponsoredProductsAdGroupOperation.getAdGroup(123)
-const sponsoredProductsAdGroupExtended = await sponsoredProductsAdGroupOperation.getAdGroupEx(123)
+// Gets an ad group specified by identifier.
+sponsoredBrandsAdGroupOperation.listAdGroups()
+
+// Sets the ad group status to archived.
+sponsoredProductsAdGroupOperation.archiveAdGroup(123)
+
+// Creates one or more ad groups.
+const sponsoredProductsAdGroup: AdGroup[] = [{
+  adGroupId: 138818764235694,
+  name: 'New Name',
+  campaignId: 31299234922913,
+  defaultBid: 1,
+  state: 'archived',
+}]
+sponsoredProductsAdGroupOperation.createAdGroups(sponsoredProductsAdGroup)
+
+// Retrieves an ad group by ID.
+sponsoredProductsAdGroupOperation.getAdGroup(123)
+
+// Retrieves an ad group and its extended fields by ID.
+sponsoredProductsAdGroupOperation.getAdGroupEx(123)
+
+// Retrieves a list of ad groups satisfying optional criteria.
+sponsoredProductsAdGroupOperation.listAdGroups()
+
+// Retrieves a list of ad groups satisfying optional criteria.
+sponsoredProductsAdGroupOperation.listAdGroupsEx()
+
+// Updates one or more ad groups. Ad groups are identified using their adGroupId
+sponsoredProductsAdGroupOperation.updateAdGroups(sponsoredProductsAdGroup)
