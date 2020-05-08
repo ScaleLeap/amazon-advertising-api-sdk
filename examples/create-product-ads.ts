4d8a77b67a246bb563cf2fd1eafff940cd6fce0f
Toan Nguyen <nguyentoandev@gmail.com>
2020-05-08 22:06:49 +0700

    chore: create product ads example

---
+++ examples/create-product-ads.ts
@@ -0,0 +1,42 @@
+import { amazonAdvertising } from './auth'
+
+import { CreateProductAdParams, UpdateProductAdParams } from '../src'
+
+const sponsoredProductsProductAdsOperation = amazonAdvertising.sponsoredProductsProductAds
+const ID = 123
+const SKU = 'AB-RED-8675309'
+
+// Sets the product ad status to archived.
+sponsoredProductsProductAdsOperation.archiveProductAd(ID)
+
+// Creates one or more product ads.
+const createProductAdParams: CreateProductAdParams[] = [
+  {
+    campaignId: ID,
+    adGroupId: ID,
+    state: 'enabled',
+    sku: SKU,
+  },
+]
+sponsoredProductsProductAdsOperation.createProductAds(createProductAdParams)
+
+// Retrieves a product ad by ID.
+sponsoredProductsProductAdsOperation.getProductAd(ID)
+
+// Retrieves a product ad and its extended fields by ID.
+sponsoredProductsProductAdsOperation.getProductAdExtended(ID)
+
+// Retrieves a list of product ads satisfying optional criteria.
+sponsoredProductsProductAdsOperation.listProductAds()
+
+// Retrieves a list of product ads with extended fields satisfying optional criteria.
+sponsoredProductsProductAdsOperation.listProductAdsExtended()
+
+// Updates one or more product ads.
+const updateProductAdParams: UpdateProductAdParams[] = [
+  {
+    adId: ID,
+    state: 'paused',
+  },
+]
+sponsoredProductsProductAdsOperation.updateProductAds(updateProductAdParams)
