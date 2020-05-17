7ee422709601d793ab81400403e095a12e546ed3
Roman Filippov <rf@romanfilippov.com>
2020-05-17 15:50:29 +0700

    fix: example

--- examples/auth.ts
+++ examples/auth.ts
@@ -1,4 +1,4 @@
-import { amazonMarketplaces } from '@scaleleap/amazon-marketplaces'
+import { amazonMarketplaces, assertMarketplaceHasAdvertising } from '@scaleleap/amazon-marketplaces'
 import { AmazonAdvertising } from '../src'
 
 const auth = {
@@ -6,4 +6,7 @@ const auth = {
   clientId: 'amzn1.application-oa2-client.a8358a60...',
   scope: 1234567890,
 }
+
+assertMarketplaceHasAdvertising(amazonMarketplaces.US)
+
 export const amazonAdvertising = new AmazonAdvertising(amazonMarketplaces.US, auth)
