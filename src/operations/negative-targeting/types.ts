2346d5460aa7792e5cf4ffc5b1c81dd250cde6e1
Toan Nguyen <nguyentoandev@gmail.com>
2020-08-24 22:36:42 +0700

    feat: implement get and update sd negative targeting method

--- src/operations/negative-targeting/types.ts
+++ src/operations/negative-targeting/types.ts
@@ -1,6 +1,14 @@
 import * as t from 'io-ts'
 import { AdGroupId } from '../ad-groups/types'
-import { TargetingClauseState, ExpressionType } from '../product-targeting/types'
+import {
+  TargetingClauseState,
+  ExpressionType,
+  TargetingClauseServingStatus,
+} from '../product-targeting/types'
+import { DateFromNumber } from 'io-ts-types/lib/DateFromNumber'
+
+export const NegativeTargetId = t.number
+export type NegativeTargetId = t.TypeOf<typeof NegativeTargetId>
 
 export const SponsoredDisplayNegativeTargetingExpressionQueryType = t.union([
   t.literal('asinBrandSameAs'),
@@ -47,4 +55,49 @@ export const CreateSponsoredDisplayNegativeTargetingClausesParams = t.strict({
 })
 export type CreateSponsoredDisplayNegativeTargetingClausesParams = t.TypeOf<
   typeof CreateSponsoredDisplayNegativeTargetingClausesParams
+>
+
+export const SponsoredDisplayNegativeTargetingClause = t.intersection([
+  CreateSponsoredDisplayNegativeTargetingClausesParams,
+  t.strict({
+    targetId: NegativeTargetId,
+  }),
+])
+export type SponsoredDisplayNegativeTargetingClause = t.TypeOf<
+  typeof SponsoredDisplayNegativeTargetingClause
+>
+
+export const SponsoredDisplayNegativeTargetingClauseExtended = t.intersection([
+  SponsoredDisplayNegativeTargetingClause,
+  t.strict({
+    /**
+     * The date the negative targeting was created as epoch time in milliseconds.
+     */
+    creationDate: DateFromNumber,
+
+    /**
+     * The date the negative targeting was last updated as epoch time in milliseconds.
+     */
+    lastUpdatedDate: DateFromNumber,
+
+    /**
+     * The computed status, accounting for out of budget, policy violations, etc. See developer notes for more information.
+     */
+    servingStatus: TargetingClauseServingStatus,
+  }),
+])
+export type SponsoredDisplayNegativeTargetingClauseExtended = t.TypeOf<
+  typeof SponsoredDisplayNegativeTargetingClauseExtended
+>
+
+export const UpdateSponsoredDisplayNegativeTargetingClausesParams = t.strict({
+  targetId: NegativeTargetId,
+
+  /**
+   * Advertiser-specified state of the negative target
+   */
+  state: TargetingClauseState,
+})
+export type UpdateSponsoredDisplayNegativeTargetingClausesParams = t.TypeOf<
+  typeof UpdateSponsoredDisplayNegativeTargetingClausesParams
 >
