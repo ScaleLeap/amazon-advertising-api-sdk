ce5fd2c231f5579240bda260f4a19e0c805a2079
Toan Nguyen <nguyentoandev@gmail.com>
2020-08-24 22:36:42 +0700

    feat: implement get and update sd negative targeting method

--- src/operations/negative-targeting/sponsored-display-negative-targeting-operation.ts
+++ src/operations/negative-targeting/sponsored-display-negative-targeting-operation.ts
@@ -1,11 +1,41 @@
 import { Operation } from '../operation'
-import { DecodeArray } from '../../decorators'
+import { Decode, DecodeArray } from '../../decorators'
 import { AmazonAdTypeURIPrefix } from '../amazon-ad-type-uri-prefix'
-import { CreateSponsoredDisplayNegativeTargetingClausesParams } from './types'
+import {
+  NegativeTargetId,
+  CreateSponsoredDisplayNegativeTargetingClausesParams,
+  SponsoredDisplayNegativeTargetingClause,
+  SponsoredDisplayNegativeTargetingClauseExtended,
+  UpdateSponsoredDisplayNegativeTargetingClausesParams,
+} from './types'
 import { NegativeTargetingClauseResponse } from '../product-targeting/types'
 
 export class SponsoredDisplayNegativeTargetingOperation extends Operation {
   protected resource = `${this.version}/${AmazonAdTypeURIPrefix.SponsoredDisplay}/negativeTargets`
+
+  /**
+   * Gets a negative targeting clause specified by identifier.
+   *
+   * @param id -
+   * @returns
+   */
+  @Decode(SponsoredDisplayNegativeTargetingClause)
+  public getNegativeTargetingClause(id: NegativeTargetId) {
+    return this.client.get<SponsoredDisplayNegativeTargetingClause>(`${this.resource}/${id}`)
+  }
+
+  /**
+   * Gets extended information for a negative targeting clause.
+   *
+   * @param id -
+   * @returns
+   */
+  @Decode(SponsoredDisplayNegativeTargetingClauseExtended)
+  public getNegativeTargetingClauseExtended(id: NegativeTargetId) {
+    return this.client.get<SponsoredDisplayNegativeTargetingClauseExtended>(
+      `${this.resource}/extended/${id}`,
+    )
+  }
 
   /**
    * Creates one or more negative targeting clauses.
@@ -18,5 +48,18 @@ export class SponsoredDisplayNegativeTargetingOperation extends Operation {
     params: CreateSponsoredDisplayNegativeTargetingClausesParams[],
   ) {
     return this.client.post<NegativeTargetingClauseResponse[]>(this.resource, params)
+  }
+
+  /**
+   * Updates one or more negative targeting clauses.
+   *
+   * @param params -
+   * @returns
+   */
+  @DecodeArray(NegativeTargetingClauseResponse)
+  public updateNegativeTargetingClauses(
+    params: UpdateSponsoredDisplayNegativeTargetingClausesParams[],
+  ) {
+    return this.client.put<NegativeTargetingClauseResponse[]>(this.resource, params)
   }
 }
