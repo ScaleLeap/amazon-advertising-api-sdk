d938ec83f4c95a80965776d5d00b09786e597ff7
sravya-yelleti <sravya.y@valuelabs.com>
2022-07-20 13:29:28 +0530

    fix: updated tests

--- test/operations/test-accounts/test-accounts-operation.test.ts
+++ test/operations/test-accounts/test-accounts-operation.test.ts
@@ -11,10 +11,10 @@ describe('TestAccountOperation', () => {
   const client = httpClientFactory()
   const operationProvider = new OperationProvider(client)
   const accountOperation = operationProvider.create(TestAccountOperation)
-  const TEST_PROFILE_ID = 2984328618318898
+  const REQUEST_ID = 'VMTZD2V14R745AHA5C4S'
 
   describe('listAccounts', () => {
-    it(`should return an array or accounts`, async () => {
+    it(`should return an array of accounts`, async () => {
       const res: Account[] = await accountOperation.listTestAccounts()
 
       expect(Array.isArray(res)).toBeTruthy()
@@ -24,79 +24,31 @@ describe('TestAccountOperation', () => {
   describe('getAccount', () => {
     it(`should return a account object`, async () => {
       expect.assertions(2)
-      const account = await accountOperation.getTestAccount(TEST_PROFILE_ID)
+      const account = await accountOperation.getTestAccount(REQUEST_ID)
 
       expect(account).toBeTruthy()
-
-      if (account) {
-        expect(account.accountId).toBe(TEST_PROFILE_ID)
-      }
     })
   })
 
-  describe('updateAccounts', () => {
-    it(`should update the account`, async () => {
+  describe('createTestAccount', () => {
+    it(`should create the test account`, async () => {
       expect.assertions(5)
-      const dailyBudget = 340
 
-      const res = await accountOperation.updateAccounts([
-        {
-          accountId: TEST_PROFILE_ID,
-          dailyBudget,
-          accountInfo: {
-            id: 'AUZWHWR0590BC',
-            marketplaceStringId: 'ATVPDKIKX0DER',
-            type: 'seller',
-            subType: 'AMAZON_ATTRIBUTION',
-          },
+      const res = await accountOperation.createTestAccount({
+        countryCode: AmazonMarketplaceAdvertisingCountryCode.IT,
+        accountType: 'VENDOR',
+        accountMetaData: {
+          vendorCode: 'ABCDE',
         },
-      ])
+      })
 
       expect(res).toBeTruthy()
       expect(res).toHaveLength(1)
 
-      if (res && res[0]) {
-        expect(res[0].code).toBe('SUCCESS')
-        expect(res[0].accountId).toBe(TEST_PROFILE_ID)
-      }
-
       await delay()
 
-      const account = await accountOperation.getAccount(TEST_PROFILE_ID)
+      const account = await accountOperation.getTestAccount(REQUEST_ID)
       expect(account).toBeTruthy()
-    })
-  })
-
-  describe('registerAccount', () => {
-    it(`should work with default params`, async () => {
-      const account = await accountOperation.registerAccount()
-      expect(account).toBeTruthy()
-    })
-  })
-
-  describe('registerBrand', () => {
-    it(`should return success`, async () => {
-      const param = {
-        countryCode: AmazonMarketplaceAdvertisingCountryCode.US,
-        brand: 'yay',
-      }
-
-      const res = await accountOperation.registerBrand(param)
-      expect(res).toBeTruthy()
-      expect(res.code).toBe('SUCCESS')
-    })
-  })
-
-  /**
-   * TODO: Need check again on Production API. Sandbox API return an error:
-   * ResourceNotFoundError: Could not find resource for full path: https://advertising-api-test.amazon.com/v2/brands
-   */
-  describe.skip('getBrands', () => {
-    it(`should return an array of brands`, async () => {
-      const [brand] = await accountOperation.getBrands()
-
-      expect(brand.brandId).toBe('2973802954634317')
-      expect(brand.brandRegistryName).toBe('yay')
     })
   })
 })
